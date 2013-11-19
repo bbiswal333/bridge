5a2993ed3b3df9ddfb0e0c64b3ccd75a459cd8df
Thomas Jansen <t.jansen@sap.com>
2013-11-19 12:47:42 +0100

    changed test server to http

--- server/server.js
+++ server/server.js
@@ -1,31 +1,7 @@
 var express = require('express');
-var https = require('https');
-var call_https = require('https');
-var fs = require('fs');
-var util = require('util');
+var http    = require('http');
+var app     = express();
 
-
-var options = {
-  key: fs.readFileSync('privatekey.pem'),
-  cert: fs.readFileSync('certificate.pem'),
-  requestCert: true
-};
-
-var app = express();
-
-app.get('/', function(req, res){
-  
-  fs.readFile('../webui/index.html',function (err, data){    
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var client_cert = req.connection.getPeerCertificate();
-    
-    res.write(data);
-    //res.write("User Name: " + util.inspect(client_cert.subject.CN, false, null));
-    //res.write("CERT: " + util.inspect(req.connection.getPeerCertificate(), false, null)); 
-    res.end();    
-    });
-});
-
+//serve static files in webui folder as http server
 app.use('/', express.static('../webui'));
-
-https.createServer(options, app).listen(443);
+http.createServer(app).listen(80);
