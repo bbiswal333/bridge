93f8872bd18f48e1b4504187de4d4df83411ee10
Daniel Schueler <daniel.schueler@sap.com>
2013-12-05 11:05:43 +0100

    - First Box Control that can dynamically load another directive. Used to implement different "Boxes"/Widgets

---
+++ webui/Boxes/TestBox/TestBoxDirective.js
@@ -0,0 +1,12 @@
+﻿dashboardBox.directive('testbox', function () {
+
+    var directiveController = ['$scope', function ($scope) {
+
+    }];
+
+    return {
+        restrict: 'E',
+        templateUrl: 'Boxes/TestBox/TestBoxDirective.html',
+        controller: directiveController
+    };
+});
