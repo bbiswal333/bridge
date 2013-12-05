8d62ee584395b59f31a45f2eeb94719bc807a034
Daniel Schueler <daniel.schueler@sap.com>
2013-12-05 14:16:44 +0100

    - ATC Chart is now a directive inside a box -> better encapsulation

---
+++ webui/Boxes/ATC/ATCDirective.js
@@ -0,0 +1,65 @@
+﻿dashboardBox.directive('atcbox', function () {
+
+    var directiveController = ['$scope', 'ATCDataProvider', 'Config', function ($scope, ATCDataProvider, Config) {
+        ATCDataProvider.getResultForConfig(Config, $scope);
+        $scope.$watch('atcData', function () { updateATCChart($scope); });
+    }];
+
+    return {
+        restrict: 'E',
+        templateUrl: 'Boxes/ATC/ATCDirective.html',
+        controller: directiveController
+    };
+});
+
+
+var updateATCChart = function ($scope) {
+    var chart1 = {};
+    chart1.type = "PieChart";
+    chart1.displayed = true;
+    chart1.cssStyle = "height:300px; width:300px;";
+    chart1.data = {
+        "cols": [
+            { id: "month", label: "Month", type: "string" },
+            { id: "laptop-id", label: "Laptop", type: "number" },
+        ], "rows": [
+            {
+                c: [
+                   { v: "Prio 1" },
+                   { v: $scope.atcData.prio1, f: $scope.atcData.prio1 + " Prio 1 messages" }
+                ]
+            },
+            {
+                c: [
+                   { v: "Prio 2" },
+                   { v: $scope.atcData.prio2, f: $scope.atcData.prio2 + " Prio 2 messages" }
+                ]
+            },
+            {
+                c: [
+                   { v: "Prio 3" },
+                   { v: $scope.atcData.prio3 }
+                ]
+            },
+            {
+                c: [
+                   { v: "Prio 4" },
+                   { v: $scope.atcData.prio4 }
+                ]
+            }
+        ]
+    };
+
+    chart1.options = {
+        "title": "ATC Errors",
+        "sliceVisibilityThreshold": 0,
+        "colors": ['red', 'orange', 'yellow', 'blue'],
+        "pieHole": 0.6,
+        "fill": 20,
+        "displayExactValues": true
+    };
+
+    chart1.formatters = {};
+
+    $scope.chart = chart1;
+}
--- webui/controller.js
+++ webui/controller.js
@@ -1,63 +1,7 @@
 'use strict';
 
-/* Controllers */
-
 var dashboardBox = angular.module('dashboardBox', ['atcServices', 'googlechart', 'ui.bootstrap']);
 
-dashboardBox.controller('Controller', ['$scope', '$http', 'ATCDataProvider', 'Config', function Controller($scope, $http, ATCDataProvider, Config) {
+dashboardBox.controller('Controller', ['$scope', '$http', function Controller($scope, $http, ATCDataProvider, Config) {
     $http.defaults.headers.common['X-Requested-With'] = undefined;
-
-    ATCDataProvider.getResultForConfig(Config, $scope);
-    $scope.$watch('atcData', function () { updateATCChart($scope); });
 }]);
-
-var updateATCChart = function ($scope) {
-    var chart1 = {};
-    chart1.type = "PieChart";
-    chart1.displayed = true;
-    chart1.cssStyle = "height:300px; width:300px;";
-    chart1.data = {
-        "cols": [
-            { id: "month", label: "Month", type: "string" },
-            { id: "laptop-id", label: "Laptop", type: "number" },
-        ], "rows": [
-            {
-                c: [
-                   { v: "Prio 1" },
-                   { v: $scope.atcData.prio1, f: $scope.atcData.prio1 + " Prio 1 messages" }
-                ]
-            },
-            {
-                c: [
-                   { v: "Prio 2" },
-                   { v: $scope.atcData.prio2, f: $scope.atcData.prio2 + " Prio 2 messages" }
-                ]
-            },
-            {
-                c: [
-                   { v: "Prio 3" },
-                   { v: $scope.atcData.prio3 }
-                ]
-            },
-            {
-                c: [
-                   { v: "Prio 4" },
-                   { v: $scope.atcData.prio4 }
-                ]
-            }
-        ]
-    };
-
-    chart1.options = {
-        "title": "ATC Errors",
-        "sliceVisibilityThreshold": 0,
-        "colors": ['red','orange', 'yellow', 'blue'],
-        "pieHole": 0.6,
-        "fill": 20,
-        "displayExactValues": true
-    };
-
-    chart1.formatters = {};
-
-    $scope.chart = chart1;
-}
