6915af7a09c97117fa2d0a0af17cc8452f8d8565
Thomas Jansen <t.jansen@sap.com>
2013-12-10 20:54:36 +0100

    routing in angular with deep linking

--- webui/controller/bridgeController.js
+++ webui/controller/bridgeController.js
@@ -1,9 +1,27 @@
-var bridgeApp = angular.module('bridgeApp', ['bridgeServices', 'googlechart', 'ui.bootstrap']);
-bridgeApp.controller('bridgeController', ['$scope', '$http', function Controller($scope, $http) {}]);
+var bridgeApp = angular.module('bridgeApp', ['bridgeServices', 'ngRoute', 'googlechart', 'ui.bootstrap']);
+
+
+bridgeApp.controller('bridgeController', ['$scope', '$http', '$route', '$location', function Controller($scope, $http, $route, $location) {
+	$location.path('/overview');
+}]);
+
+bridgeApp.controller('bridgeControllerOverview', ['$scope', '$http', '$route', function Controller($scope, $http, $route) {
+	$scope.overview = true;
+}]);
+
+bridgeApp.controller('bridgeControllerDetail', ['$scope', '$http', '$route', function Controller($scope, $http, $route) {
+	$scope.back = function() {
+		$location.path('/overview');
+	};
+}]);
+
+
+bridgeApp.config(function($routeProvider) {
+
+	$routeProvider.when("/overview", {templateUrl:'view/overview.html', controller:'bridgeControllerOverview'});
+	$routeProvider.when("/detail", {templateUrl:'view/detail.html', controller:'bridgeControllerDetail'});
+
+});
+
+
 var bridgeServices = angular.module('bridgeServices', ['ngResource']);
-
-
-
-
-
-
