b0e854ac26f677bfb9b35e1aa2a9c23c1b84e499
Daniel Schueler <daniel.schueler@sap.com>
2013-12-12 15:55:39 +0100

    missing files added

---
+++ webui/app/atcBox/ATCBoxDetailsController.js
@@ -0,0 +1,3 @@
+﻿bridgeApp.controller('atcDetailController', ['$scope', '$http', '$route', '$routeParams', function Controller($scope, $http, $route, $routeParams) {
+    $scope.$parent.titleExtension = " - ATC Details";
+}]);
