f15db443c9a920daf71b756f3d4a38e8ab48544c
Daniel Schueler <daniel.schueler@sap.com>
2014-01-21 16:19:28 +0100

    ngTable first steps on settings screen

--- webui/app/atcBox/ATCBoxSettingsController.js
+++ webui/app/atcBox/ATCBoxSettingsController.js
@@ -1,5 +1,17 @@
-﻿atcApp.settingsController = function ($scope, Config) {
-    $scope.content = "Content From Controller";
-
+﻿atcApp.settingsController = function ($scope, $filter, ngTableParams, Config) {
     $scope.config = Config;
+
+    $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+    }, {
+        total: $scope.config.configItems.length,
+        getData: function($defer, params) {
+            var orderedData = params.sorting() ?
+                    $filter('orderBy')($scope.config.configItems, params.orderBy()) :
+                    $scope.config.configItems;
+    
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
 };
