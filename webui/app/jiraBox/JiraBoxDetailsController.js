e461397abeaa504b068fcaad8de76cef887d6657
Thomas Jansen <t.jansen@sap.com>
2014-01-23 17:01:44 +0100

    jira box cleanup

--- webui/app/jiraBox/JiraBoxDetailsController.js
+++ webui/app/jiraBox/JiraBoxDetailsController.js
@@ -1,5 +1,5 @@
 ﻿jiraApp.controller('jiraDetailController', ['$scope', '$http', '$filter', '$route', '$routeParams', 'ngTableParams', 'Config', 'JiraQuery', 'JiraBox',
     function Controller($scope, $http, $filter, $route, $routeParams, ngTableParams, Config, JiraQuery, JiraBox) {
         $scope.$parent.titleExtension = " - Jira Details";
-        //JiraBox.getIssuesforQuery(JiraQuery, $scope);
+        JiraBox.getIssuesforQuery(JiraQuery, $scope);
 }]);
