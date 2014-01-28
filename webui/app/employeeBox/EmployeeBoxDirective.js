3ab92a94ae158502d102d9203de1b7d3fbdb5dcc
Thomas Jansen <t.jansen@sap.com>
2014-01-28 17:43:34 +0100

    settings in box layout and employee box adjustments

--- webui/app/employeeBox/EmployeeBoxDirective.js
+++ webui/app/employeeBox/EmployeeBoxDirective.js
@@ -16,6 +16,25 @@ testBoxApp.directive('employeebox', function () {
         
     }];
 
+    //get details for employee like email, sapname, telefone number, inhouse mail, manager, mobile number
+    /*$scope.$watch('selectedEmployee', function () { 
+        if(selectedEmployee != undefined && selectedEmployee.BNAME != undefined)
+        {
+
+                this.http.get('http://localhost:8000/api/get?url=' + encodeURIComponent('https://ifp.wdf.sap.corp/sap/bc/devdb/find_employee?id=' + config.getQueryString() + ''))
+        .success(function (data) {
+
+            dataService.data = {
+                prio1: data.PRIOS.PRIO1,
+                prio2: data.PRIOS.PRIO2,
+                prio3: data.PRIOS.PRIO3,
+                prio4: data.PRIOS.PRIO4,
+            };
+
+            $scope.EmployeeDetails
+        }
+    });*/
+
     return {
         restrict: 'E',
         templateUrl: 'app/employeeBox/EmployeeBoxDirective.html',
