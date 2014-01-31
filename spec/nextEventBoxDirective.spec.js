5af27fdc8716d6707632b1f5172bda44763ee4dd
d059349 <florian.loch@sap.com>
2014-01-31 18:19:34 +0100

    Added relative/absolute time display, respect timezones - a lot of stuff has been done here :)

--- spec/nextEventBoxDirective.spec.js
+++ spec/nextEventBoxDirective.spec.js
@@ -48,7 +48,40 @@ describe("nextEventBoxDirective ewsUrlBuilder test", function () {
 		var expectation = new RegExp(".*\?from=" + encodeForUrl("2014-02-28T00:00:00Z") + "&to=" + encodeForUrl("2014-03-01T00:00:00Z") + "&format=json$");
 
 		expect(url).toMatch(expectation);
+	});
+
+
+
+	it("should be tested by a working test, so test RegEx first", function () {
+		expect("2014-02-28T00:00:00Z").toMatch(/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z$/);
+	});
+
+	it("it should return null if the given date string is invalid", function () {
+		expect(ewsUrlBuilder.parseEWSDateString("2014-02-28BLA00:00:00Z")).toBeNull();
+	});
+
+	it("should parse a date string from Outlook correctly", function () {
+		var expected = new Date(2014, 1, 28, 11, 9, 44).getTime(); //1 is February
+		var expected2 = new Date(2014, 1, 28, 23, 9, 44).getTime(); //1 is February
+
+
+		expect(ewsUrlBuilder.parseEWSDateString("2014-02-28T10:09:44Z", 1).getTime()).toEqual(expected); //1 for UTC+1, therefore 10 not 11
+		expect(ewsUrlBuilder.parseEWSDateString("2014-02-28T12:09:44Z", -1).getTime()).toEqual(expected); //-1 for UTC-1, therefore 12 not 11
+		expect(ewsUrlBuilder.parseEWSDateString("2014-03-01T01:09:44Z", -2).getTime()).toEqual(expected2); //-2 for UTC-2, therefore 23 not 1
 	});	
+
+	it("should parse a date string and calculate my current time-zone (UTC+1) into it", function () {
+		var expected = new Date(2014, 0, 10, 13, 9, 44).getTime(); 
+
+		expect(ewsUrlBuilder.parseEWSDateStringAutoTimeZone("2014-01-10T12:09:44Z").getTime()).toEqual(expected);
+	});
+
+
+	it("should calculate relative time between two date objects", function () {
+		expect(ewsUrlBuilder.relativeTimeTo(new Date(2014, 10, 9, 21, 19, 0), new Date(2014, 10, 10, 22, 20, 10))).toEqual("1 day, 1 hour, 1 minute");
+		expect(ewsUrlBuilder.relativeTimeTo(new Date(2014, 10, 10, 20, 20, 0), new Date(2014, 10, 10, 22, 20, 0))).toEqual("2 hours, 0 minutes");
+		expect(ewsUrlBuilder.relativeTimeTo(new Date(2014, 10, 6, 20, 20, 0), new Date(2014, 10, 10, 22, 20, 0))).toEqual("4 days, 2 hours, 0 minutes");
+	});
 });
 
 function encodeForUrl(val_s) {
--- webui/app/nextEventBox/nextEventBoxDirective.js
+++ webui/app/nextEventBox/nextEventBoxDirective.js
@@ -9,11 +9,11 @@ angular.module("nextEventBoxApp", []).factory("ewsUrlBuilder", function () {
 	
 		function buildDateString (date_o) {
 			var year = date_o.getFullYear();
-			var month = useNDigits(date_o.getMonth() + 1, 2);
-			var day = useNDigits(date_o.getDate(), 2);
-			var hour = useNDigits(date_o.getHours(), 2);
-			var minute = useNDigits(date_o.getMinutes(), 2);
-			var second = useNDigits(date_o.getSeconds(), 2);
+			var month = _useNDigits(date_o.getMonth() + 1, 2);
+			var day = _useNDigits(date_o.getDate(), 2);
+			var hour = _useNDigits(date_o.getHours(), 2);
+			var minute = _useNDigits(date_o.getMinutes(), 2);
+			var second = _useNDigits(date_o.getSeconds(), 2);
 
 			return year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second + "Z"; 
 		}
@@ -21,22 +21,77 @@ angular.module("nextEventBoxApp", []).factory("ewsUrlBuilder", function () {
 		function encodeForUrl (val_s) {
 			return encodeURIComponent(val_s).replace(/'/g,"%27").replace(/"/g,"%22");
 		}
+	}
 
 		//Testes by a separate test
-		function useNDigits (val_i, n_i) {
-			var str = new String(val_i);
+	function _useNDigits (val_i, n_i) {
+		var str = new String(val_i);
 
-			for (var i = str.length; i < n_i; i++) {
-				str = "0" + str;
-			}
-
-			return str;
+		for (var i = str.length; i < n_i; i++) {
+			str = "0" + str;
 		}
+
+		return str;
+	}
+
+	function _parseEWSDateString (ewsDateStr_s, offsetUTC_i) {
+		var s = ewsDateStr_s;
+
+		//Check whether this string seems to be valid
+		if (s.search(/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z$/) == -1) {
+			return null;
+		}
+
+		var year = s.substr(0, 4);
+		var month = parseInt(s.substr(5, 2)) - 1;
+		var day = s.substr(8, 2);
+		var hour = s.substr(11, 2);
+		var minute = s.substr(14, 2);
+		var second = s.substr(17, 2);
+
+		var d = new Date(year, month, day, hour, minute, second);
+
+		return new Date(d.getTime() + (offsetUTC_i * 3600000)); //3600000 milliseconds are one hour
 	}	
+
+	function _relativeTimeTo(dateFrom_o, dateTo_o) {
+		var diffMin = dateTo_o.getTime() - dateFrom_o.getTime();
+		diffMin = Math.floor(diffMin / 60000);
+
+		var days = Math.floor(diffMin / (24 * 60));
+		diffMin = diffMin - days * 24 * 60;
+
+		var hours = Math.floor(diffMin / 60);
+		diffMin = diffMin - hours * 60;
+
+		var res = "";
+		if (days > 0) {
+			res += days + ((days == 1) ? " day, " : " days, ");
+		}
+		if (hours > 0) {
+			res += hours + ((hours == 1) ? " hour, " : " hours, ");
+		}
+		res += diffMin + ((diffMin == 1) ? " minute" : " minutes");
+
+		return res;
+	}
+
 
 	return {
 		buildEWSUrl: function(dateFrom_o, days_i) {
 			return _buildEWSUrl(dateFrom_o, days_i);
+		},
+		parseEWSDateString: function (ewsDateStr_s, offsetUTC_i) {
+			return _parseEWSDateString(ewsDateStr_s, offsetUTC_i);		
+		},
+		parseEWSDateStringAutoTimeZone: function (ewsDateStr_s) {
+			return _parseEWSDateString(ewsDateStr_s, (new Date().getTimezoneOffset() / -60));			
+		},
+		relativeTimeTo: function (dateFrom_o, dateTo_o) {
+			return _relativeTimeTo(dateFrom_o, dateTo_o);
+		},
+		useNDigits: function (val_i, n_i) {
+			return _useNDigits (val_i, n_i);
 		}
 	};
 }).	directive("nexteventbox", function ($http, ewsUrlBuilder) {
@@ -50,7 +105,7 @@ angular.module("nextEventBoxApp", []).factory("ewsUrlBuilder", function () {
 		$scope.currentEvent = 0;
 		$scope.nextEvents = [];
 		$scope.dayCnt = 3;
-		$scope.loading = false;
+		$scope.loading = true;
 
 		/*$scope.$watch('dayCnt', function(newValue, oldValue, scope) {
 			if (newValue != "" && newValue > 0) {
@@ -60,18 +115,38 @@ angular.module("nextEventBoxApp", []).factory("ewsUrlBuilder", function () {
 
 		function loadFromExchange () {
 			$scope.loading = true;
+			var dateFn = ewsUrlBuilder.parseEWSDateStringAutoTimeZone;
+			var today = new Date();
+			today.setHours(0);
+			today.setMinutes(0);
+			today.setSeconds(0);
+
 			$http.get(ewsUrlBuilder.buildEWSUrl(new Date(), $scope.dayCnt)).success(function (data, status) {
 				calData = eval(data);
 
 				events = calData["s:Envelope"]["s:Body"][0]["m:FindItemResponse"][0]["m:ResponseMessages"][0]["m:FindItemResponseMessage"][0]["m:RootFolder"][0]["t:Items"][0]["t:CalendarItem"];
+				console.log(events);
 				$scope.nextEvents = [];
 				if (typeof events != "undefined") {
+					var j = 0;
 					for (var i = 0; i < events.length; i++) {
-						$scope.nextEvents[i] = {
+						//ignore events, which are over already
+						if (dateFn(events[i]["t:End"][0]).getTime() <= new Date().getTime()) {
+							continue;
+						}
+
+						console.log(events[i]["t:End"][0]);
+						console.log("to");
+						console.log(dateFn(events[i]["t:End"][0]));
+
+						$scope.nextEvents[j] = {
 							subject: events[i]["t:Subject"][0],
-							start: events[i]["t:Start"][0],
-							end: events[i]["t:End"][0]
+							start: dateFn(events[i]["t:Start"][0]),
+							end: dateFn(events[i]["t:End"][0]),
+							timeZone: events[i]["t:TimeZone"][0]
 						};
+
+						j++;
 					}
 				}
 
@@ -80,8 +155,34 @@ angular.module("nextEventBoxApp", []).factory("ewsUrlBuilder", function () {
 			});
 		};
 
-		$scope.getCurrentEvent = function () {
+		$scope.getCurrentEventAbsolute = function () {
 			return $scope.nextEvents[$scope.currentEvent];
+		};
+
+		$scope.getCurrentEventRelative = function () {
+			var evt = $scope.getCurrentEventAbsolute();
+			var ret = {};
+			ret.subject = evt.subject;
+			ret.start = ewsUrlBuilder.relativeTimeTo(new Date(), evt.start);
+			ret.end = ewsUrlBuilder.relativeTimeTo(new Date(), evt.end);
+
+			return ret;
+		};
+
+		$scope.getCurrentEvent = function () {
+			var evt = $scope.getCurrentEventAbsolute();
+			if ((evt.start.getTime() - new Date()) >= (2 * 24 * 3600000)) {
+				var ret = {};
+				ret.subject = evt.subject;
+
+				//n digits
+				ret.start = evt.start.getHours() + ":" + evt.start.getMinutes() + ", " + evt.start.getDay() + "." + (evt.start.getMonth() + 1);
+				ret.end = evt.end.getHours() + ":" + evt.end.getMinutes() + ", " + evt.end.getDay() + "." + (evt.end.getMonth() + 1);
+				return ret;
+			}
+			else {
+				return $scope.getCurrentEventRelative();
+			}
 		};
 
 		$scope.showNextEvent = function () {
