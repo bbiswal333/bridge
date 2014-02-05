c9d5908661ee6b37ccddce98e2d025fb88b1fbab
d059349 <florian.loch@sap.com>
2014-02-05 13:29:14 +0100

    Added factory for easier mycats-access

--- webui/app/catsMiniCalBox/catsMiniCalDirective.js
+++ webui/app/catsMiniCalBox/catsMiniCalDirective.js
@@ -148,10 +148,35 @@ angular.module("catsMiniCalBoxApp", []).directive("catsminicalbox", function (ca
 			return _weekdays;
 		}
 	};
-}).factory("catsDataHelper", function ($http) {
-	//function _requestCatsData
+}).factory("catsDataRequest", function ($http, encodeForUrl) {
+	const NODE_GET_API = "http://localhost:8000/api/get?url=";
+	const CATS_WEBSERVICE = "https://isp.wdf.sap.corp/sap/bc/zdevdb/MYCATSDATA";
+	var url = NODE_GET_API + encodeForUrl(CATS_WEBSERVICE) + "&json=true";
 
-	//$http.get(ewsHelperUtils.buildEWSUrl(new Date(), $scope.dayCnt)).success(function (data, status) {
+	function _requestCatsData (callback_fn) {
+		$http.get(url).success(function (data, status) {
+			if (between(status, 200, 299)) {
+				callback_fn(JSON.parse(data));
+			}
+		}).error(function (data, status, header, config) {
+			console.log("GET-Request to " + url + " failed. HTTP-Status: " + status + ".\nData provided by server: " + data);
+			callback_fn(null);
+		});
 
+		function between (val_i, min_i, max_i) {
+			return (val_i >= min__i && val_i <= max_i);
+		}
+	}
 
+	return {
+		getData: function () {		//Returns either an object generated from json string or null in case Request wasn't successful. In the last case the method will internaly invoke a console.log()
+			return _requestCatsData();
+		}
+	};
+}).factory("encodeForUrl", function () {
+	return {
+		encode: function (url_s) {
+			return encodeURIComponent(val_s).replace(/'/g,"%27").replace(/"/g,"%22");
+		}
+	};
 });
