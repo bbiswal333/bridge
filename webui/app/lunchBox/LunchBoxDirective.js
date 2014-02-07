d2ff6ed5dd99ed513d723ea16bcf7af92196b4a5
Thomas Jansen <t.jansen@sap.com>
2014-02-07 16:06:02 +0100

    lunch app first release

--- webui/app/lunchBox/LunchBoxDirective.js
+++ webui/app/lunchBox/LunchBoxDirective.js
@@ -14,9 +14,21 @@ testBoxApp.directive('lunchbox', function () {
         };
 
         
-        $http.get('http://localhost:8000/api/get?url=' + encodeURI('http://155.56.69.85:1081/lunch_de.txt')
-        ).success(function(data, status, headers, config) {
-            $scope.lunch = data;
+        $http.get('http://localhost:8000/api/get?url=' + encodeURI('http://155.56.69.85:1081/lunch_de.txt') + '&decode=win1252'
+        ).success(function(data, status, headers, config) {            
+            
+            var weekday = new Date().getDay() - 1;
+            var lunchstring = data.split('************')[weekday];
+            $scope.lunch = lunchstring;        
+
+            /*NSRange soupRange = [text rangeOfString:de?@"Suppe:":@"Soup:"];
+            NSRange main1Range = [text rangeOfString:de?@"Hauptgericht:":@"Main course:"];
+            NSRange main2Range = [text rangeOfString:de?@"Oder:":@"Or:"];
+            NSRange sideDishesRange = [text rangeOfString:de?@"Beilagen:":@"Side dishes:"];
+            NSRange dessertRange = [text rangeOfString:de?@"Dessert:":@"Dessert:"];
+            NSRange header = [text rangeOfString:de?@"Speiseplan Walldorf":@"Weekly Menu Walldorf"];
+            NSRange resultRange;*/
+
         }).error(function(data, status, headers, config) {
             console.log(data);
         });
