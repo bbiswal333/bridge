745ae66309cc66c4c5b9654fd64e294344d23556
Rene Laengert <rene.laengert@sap.com>
2014-02-14 13:50:00 +0000

    ozarcs test

--- webui/counter.js
+++ webui/counter.js
@@ -5,6 +5,7 @@
 		loggingEnabled: true,
 		visitorCookieDuration: 7776000000
 	};
+	
 	(function(){
 		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 		g.type='text/javascript'; g.defer=true; g.async=true; g.src=clickstream.baseUrl+'js/privacy.js';
