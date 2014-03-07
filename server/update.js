2a9d6994644a753b41df093b685a278f5b8d6a8d
Thomas Jansen <t.jansen@sap.com>
2014-03-07 09:13:53 +0100

    added gith

---
+++ server/update.js
@@ -0,0 +1,27 @@
+var param 		= require('./params.js');
+var npm_load	= require('./npm_load.js');
+
+exports.run = function(npm)
+{	
+	var port   = param.get("port", 8001);
+	var proxy  = param.get("proxy", true);	
+	
+	function start_server()
+	{
+		var gith = require('gith').create( port );
+		console.log("Git Hook Server started.");
+
+		gith({
+  			repo: 'D050049/test',
+  			branch: 'master'
+		}).on( 'all', function( payload ) {
+  			console.log( payload );  			
+		});
+
+	
+	}
+	
+	npm_load.get(proxy, npm, start_server);
+}
+
+if(require.main === module){ exports.run('npm'); }
