e52cef5967b9214f703328e79970c84fb0fa7535
Thomas Jansen <t.jansen@sap.com>
2014-03-10 08:23:35 +0100

    small adjustments

--- webui/Browser.js
+++ webui/Browser.js
@@ -6,11 +6,11 @@ navigator.sayswho = function(){
         return 'IE '+(tem[1] || '');
     }
     M= M[2]? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
-    if((tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
+    if((tem= ua.match(/version\/([\.\d]+)/i))!== null) M[2]= tem[1];
     return M;
 };
 
 if (navigator.sayswho()[0] != "Chrome" && navigator.sayswho()[0] != "Firefox" && navigator.sayswho()[0] != "Safari" && navigator.sayswho()[0] == "MSIE" && parseFloat(navigator.sayswho()[1]) < 10)
 {
 	location.href="/browser_not_supported.html";
-};
+}
