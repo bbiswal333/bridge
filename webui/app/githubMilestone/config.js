a798ad7911835b1d85e0d9ba38662b5349565c64
Jan Gottweiss <jan.gottweiss@sap.com>
2014-03-10 10:19:17 +0100

    responsive and centered tiles

--- webui/app/githubMilestone/config.js
+++ webui/app/githubMilestone/config.js
@@ -3,9 +3,9 @@ angular.module('app.githubMilestone').factory("app.githubMilestone.configservice
 	var configItem = {
 		repo: {
 					name:"bridge",
-					full_name: "Tools/bridge", 
-					html_url: "https://github.wdf.sap.corp/Tools/bridge",
-					api_url: "https://github.wdf.sap.corp/api/v3/Tools/bridge",
+					full_name: "bridge/bridge", 
+					html_url: "https://github.wdf.sap.corp/bridge/bridge",
+					api_url: "https://github.wdf.sap.corp/api/v3/bridge/bridge",
 			},	
 
 		milestoneDuration: 7, 	
