a77b94197c2e9680ef03cded753e8d265deec3cc
Thomas Jansen <t.jansen@sap.com>
2014-03-17 13:27:12 +0100

    enhanced browser detection using modernizr closes #135

--- webui/browser.js
+++ webui/browser.js
@@ -1,28 +1,23 @@
-navigator.sayswho = function()
+navigator.featuresAvailable = function()
 {
-    var ua= navigator.userAgent, tem,
-    M= ua.match(/(opera|chrome|safari|firefox|msie|trident|iceweasel(?=\/))\/?\s*([\d\.]+)/i) || [];
-    if(/trident/i.test(M[1])){
-        tem=  /\brv[ :]+(\d+(\.\d+)?)/g.exec(ua) || [];
-        return 'IE '+(tem[1] || '');
-    }
-    M= M[2]? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
-    if((tem= ua.match(/version\/([\.\d]+)/i))!== null) M[2]= tem[1];
-    return M;
-};
+    var feature_check = [];
 
-navigator.browserValid = function()
-{            
-    if (navigator.sayswho()[0] == "Chrome" && parseFloat(navigator.sayswho()[1]) >= 32)     return true;
-    if (navigator.sayswho()[0] == "Firefox" && parseFloat(navigator.sayswho()[1]) >= 17)    return true;
-    if (navigator.sayswho()[0] == "Safari" && parseFloat(navigator.sayswho()[2]) >= 7)      return true;
-    if (navigator.sayswho()[0] == "Safari" && parseFloat(navigator.sayswho()[3]) >= 7)      return true;
-    if (navigator.sayswho()[0] == "Iceweasel" && parseFloat(navigator.sayswho()[1]) >= 2)   return true;    
-    if (navigator.sayswho() == "IE 11.0")                                                   return true;
-    return false;
-};
+    feature_check.push( Modernizr.cookies );
+    feature_check.push( Modernizr.opacity );
+    feature_check.push( Modernizr.borderradius );
+    feature_check.push( Modernizr.cors );
+    feature_check.push( Modernizr.fontface );
+    feature_check.push( Modernizr.svg );
+    feature_check.push( Modernizr.inlinesvg );
+    feature_check.push( Modernizr.cssanimations );
+    feature_check.push( Modernizr.csstransforms );
+    feature_check.push( Modernizr.cssgradients );
+    feature_check.push( Modernizr.geolocation );    
 
-if(!navigator.browserValid())
+    return feature_check.every(Boolean);    
+}
+
+if( !navigator.featuresAvailable() )
 {
     location.href="/browser_not_supported.html";
 }
