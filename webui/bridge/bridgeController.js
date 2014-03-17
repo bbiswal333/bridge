6aad403b394a0251d25ea1ebeaeecf86ca2c236a
D049583 <rene.laengert@sap.com>
2014-03-17 15:02:36 +0100

    introduce new counter

--- webui/bridge/bridgeController.js
+++ webui/bridge/bridgeController.js
@@ -130,9 +130,16 @@ angular.module('bridge.app').controller('bridgeController', ['$scope', '$http', 
                 {
                     $scope.apps = sortableConfig.getDefaultConfig();
                 }
-                    $scope.configLoadingFinished = true;
-                    $scope.showLoadingAnimation = false;   
-                });
+                var URL = location.href;
+                URL = URL.replace("https://","");
+                URL = URL.replace("http://","");
+                $http.get('https://ifd.wdf.sap.corp:443/sap/bc/zxa/COLLECT_WEB_STATS?URL=' + encodeURIComponent( URL ) +
+                                                                                    '&SECTION=' + 'MAIN' +
+                                                                                    '&ACTION=' + 'PAGELOAD' +
+                                                                                    '&origin=' + location.origin);
+                $scope.configLoadingFinished = true;
+                $scope.showLoadingAnimation = false;   
+            });
         }]);
 
 
