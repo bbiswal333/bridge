d3b4a90479685c5990f7745a39ed9e8540d9f6d3
Christian von Rehberg aka Jeschke <christian.jeschke@sap.com>
2014-04-01 21:25:59 +0200

    moved calendar into separated app.cats.calendar directive -> fixed bug, that name and icon of the app are not showing up

--- webui/bridge/sortableService.js
+++ webui/bridge/sortableService.js
@@ -2,7 +2,6 @@ angular.module('bridge.app').service('sortableConfig', function () {
 
 	this.getDefaultConfig = function(){
 		return [                                        
-            {content: "app.cats.calendar", id: 1, size:"box-2", show:true},
             {content: "app.lunch-walldorf", id: 2, size:"box-2", show:true},
             {content: "app.jira", id: 3, size:"box-2", show:true},
 
@@ -13,7 +12,8 @@ angular.module('bridge.app').service('sortableConfig', function () {
             {content: "app.github-milestone", id: 7, size:"box-2", show:true},
             {content: "app.im", id: 8, size:"box-1", show:true},
             {content: "app.link-list", id: 9, size:"box-1", show:true} ,
-            {content: "app.wire", id: 10, size:"box-1", show:true}
+            {content: "app.wire", id: 10, size: "box-1", show: true },
+            {content: "app.cats", id: 1, size: "box-2", show: true },
         ]; 		
 	}
 
