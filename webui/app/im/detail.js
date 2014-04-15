c2c2782384e0c132734ea2ea35d898ba6ac288ac
Thomas Jansen <t.jansen@sap.com>
2014-04-15 10:51:05 +0200

    im cors fix

--- webui/app/im/detail.js
+++ webui/app/im/detail.js
@@ -7,13 +7,12 @@ angular.module('app.im').controller('app.im.detailController', ['$scope', '$http
 	        ).success(function(data) {
 	        	data = new X2JS().xml_str2json(data);
                 $scope.imData = data["abap"];
-                $scope.imData = $scope.imData["values"];
-	            $scope.imData = $scope.imData[0];
+                $scope.imData = $scope.imData["values"];	            
 	       	 	$scope.tempobject = [];
 
-	       	   	if ($scope.imData.INTCOMP_LONG[0] !== "") {
-	       	   		_.each($scope.imData.INTCOMP_LONG[0].DEVDB_MESSAGE_OUT, function (n) {
-	       	   			$http.get('https://ifp.wdf.sap.corp:443/sap/bc/zxa/FIND_EMPLOYEE_JSON?id=' + n.SUSID[0] + '&origin=' + location.origin).then(function (response) {
+	       	   	if ($scope.imData.INTCOMP_LONG !== "") {
+	       	   		_.each($scope.imData.INTCOMP_LONG.DEVDB_MESSAGE_OUT, function (n) {
+	       	   			$http.get('https://ifp.wdf.sap.corp:443/sap/bc/zxa/FIND_EMPLOYEE_JSON?id=' + n.SUSID + '&origin=' + location.origin).then(function (response) {
 	                        n.employee = response.data.DATA;
 	                        n.employee.TELNR = n.employee.TELNR_DEF.replace(/ /g, '').replace(/-/g, '');
                        		$scope.tempobject.push(n);
