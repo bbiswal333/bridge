2277cde8b538877393b0db85639b356b0429c158
Thomas Jansen <t.jansen@sap.com>
2014-04-15 16:29:35 +0200

    github collaboration

--- webui/bridge/bridgeController.js
+++ webui/bridge/bridgeController.js
@@ -70,7 +70,13 @@ angular.module('bridge.app').controller('bridgeController',
         }
 
         $scope.bridge_github_click = function () {
-            $window.open("https://github.wdf.sap.corp/bridge/bridge");
+            $scope.sidePanel = 'view/bridgegithub.html';
+            if($scope.sideView == "github" || !$scope.show_settings)
+            {
+                $scope.show_settings = !$scope.show_settings;
+            }
+            $scope.sideView = "github";    
+            //$window.open("https://github.wdf.sap.corp/bridge/bridge");
         }
 
         $scope.bridge_hide_settings = function(){
