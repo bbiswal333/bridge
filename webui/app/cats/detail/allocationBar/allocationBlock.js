6b93a1fcf77b042728393f563bb594d4681c2723
D049583 <rene.laengert@sap.com>
2014-04-28 14:44:48 +0200

    remove the remove icon from allocation bar

--- webui/app/cats/detail/allocationBar/allocationBlock.js
+++ webui/app/cats/detail/allocationBar/allocationBlock.js
@@ -29,12 +29,12 @@ angular.module("app.cats.allocationBar.core.block", []).factory("app.cats.alloca
 
             divJQuery.append($("<div>").addClass('allocation-bar-block-name-div').text(desc_s).attr("title", desc_s));
             divJQuery.append($("<div>").addClass('allocation-bar-block-value-div'));
-            if (!this.isFixed) {
+            /*if (!this.isFixed) {
                 divJQuery.append($("<div>").addClass('allocation-bar-block-bin-div').hide().html('<span class="glyphicon glyphicon-remove"></span>').click(function () {
                     self.remove();
                     self.p.fireAllocChanged();
                 }));
-            }
+            }*/
             divJQuery.hover(function () {
                 if (!binFadedIn) $(this).children(".allocation-bar-block-bin-div").fadeIn();
                 binFadedIn = true;
