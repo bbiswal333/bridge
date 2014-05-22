bddbad4156a68a09839a5d8f618ec463638f1f02
D051804 <daniel.schueler@sap.com>
2014-05-22 16:35:58 +0200

    fist steps for new CATS Data Model

--- webui/app/cats/catsMonthlyData.js
+++ webui/app/cats/catsMonthlyData.js
@@ -1,11 +1,24 @@
-angular.module("app.cats.monthlyDataModule")
+angular.module("app.cats.monthlyDataModule", [])
 
-.service("app.cats.monthlyData", ["$http", function($http){
+.service("app.cats.monthlyData", ["$http", "$q",  function($http, $q){
 	this.getMonthlyData = function(){
 
 	};
 
-	this.getWeeklyData = function(){
+	this.getWeeklyData = function (year, week) {
+	    var deferred = $q.defer();
+
+	    $http.get('https://isp.wdf.sap.corp/sap/bc/zdevdb/GETCATSDATA?format=json&origin=' + location.origin + '&week=' + year + '.' + week)
+	    .success(function (data) {
+	        this.convertWeekData(data);
+	        deferred.resolve(parsedData);
+	    });
+
+	    return deferred.promise;
+	};
+
+	this.convertWeekData = function (backendData) {
 
 	};
 }]);
+
