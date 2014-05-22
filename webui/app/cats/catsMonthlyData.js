fbee2874b23cf6fd327d9afff7b99794fd8b519c
D049583 <rene.laengert@sap.com>
2014-05-22 14:44:30 +0200

    [lunch] fix for more robustness (current data is wrong)

---
+++ webui/app/cats/catsMonthlyData.js
@@ -0,0 +1,11 @@
+angular.module("app.cats.monthlyDataModule")
+
+.service("app.cats.monthlyData", ["$http", function($http){
+	this.getMonthlyData = function(){
+
+	};
+
+	this.getWeeklyData = function(){
+
+	};
+}]);
