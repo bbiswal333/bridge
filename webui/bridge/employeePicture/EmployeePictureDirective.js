4ae1805351f56cb2c11b8d9ba6b2b788bf62f800
d049677 <christian.jeschke@sap.com>
2014-05-26 16:01:27 +0200

    Changed settings, employee picture and backend.
    It's now possible to select ATC results only by responsible.
    
    closes #237

--- webui/bridge/employeePicture/EmployeePictureDirective.js
+++ webui/bridge/employeePicture/EmployeePictureDirective.js
@@ -3,7 +3,7 @@
 angular.module('bridge.employeePicture').directive('bridge.employeePicture', function () {
     return {
         restrict: 'E',
-        template: '<img style=" border-radius:50%;-moz-border-radius:50%;-webkit-border-radius:50%;" valign="top" ng-src="https://avatars.wdf.sap.corp/avatar/{{employeeUser}}?s={{width || 25}}x{{height || 25}}"></img>',
+        template: '<img style="border-radius:50%;-moz-border-radius:50%;-webkit-border-radius:50%;" ng-if="employeeUser" valign="top" ng-src="https://avatars.wdf.sap.corp/avatar/{{employeeUser}}?s={{width || 25}}x{{height || 25}}"></img>',
         scope: {
             employeeUser: '@',
             width: '=',
