f795f4387f9ef0d3537ebde8230c77ba37ea8dcf
Thomas Jansen <t.jansen@sap.com>
2014-06-04 13:26:20 +0200

    jira filter buttons

--- webui/app/jira/detail.js
+++ webui/app/jira/detail.js
@@ -6,26 +6,63 @@
             JiraBox.getIssuesforQuery($scope);            
         },true);
 
+        $scope.data = {};
+        $scope.data.filteredJiraData = [];
+        $scope.data.status = [];
+
         $scope.$watch('data.jiraData', function()
         {
             var status_filter = $routeParams['status'].split('|');
         	if($scope.data && $scope.data.jiraData)
         	{
-	        	$scope.data.filteredJiraData = [];
+	        	//get status values and selected ones
+                var all_status = [];
+                var status = [];
 	        	for(var i = 0; i < $scope.data.jiraData.length; i++ )
 	        	{
-	        		if(status_filter.indexOf($scope.data.jiraData[i].status) > -1)
-	        		{
-	        			$scope.data.filteredJiraData.push($scope.data.jiraData[i]);
-	        		}
+                    if( !(all_status.indexOf($scope.data.jiraData[i].status) > -1 ))
+                    {
+                        all_status.push($scope.data.jiraData[i].status);
+                    }
 	        	}
+
+                all_status.sort();
+                
+                for(var i = 0; i < all_status.length; i++ )
+                {
+                    if(status_filter.indexOf(all_status[i]) > -1)
+                    {
+                        status.push({"name":all_status[i],"active":true});
+                    }
+                    else
+                    {
+                        status.push({"name":all_status[i],"active":false});   
+                    }
+                }  
+                $scope.data.status = status;
         	}
         },true);
 
-        $scope.$parent.titleExtension = " - Jira Details"; 
+        $scope.$watch('data.status', function()
+        {
+            $scope.data.filteredJiraData = [];
+            if($scope.data && $scope.data.jiraData)
+            {
+                for(var i = 0; i < $scope.data.jiraData.length; i++ )
+                {
+                    for(var j = 0; j < $scope.data.status.length; j++)
+                    {
+                        if($scope.data.jiraData[i].status == $scope.data.status[j].name && $scope.data.status[j].active)
+                        {
+                            $scope.data.filteredJiraData.push($scope.data.jiraData[i]);
+                        }
+                    }
+                }
+            }
+        }, true);
 
-        $scope.data = {};
-        $scope.data.filteredJiraData = [];
+
+        $scope.$parent.titleExtension = " - Jira Details"; 
 
         var cellTemplate = '<div class="ngCellText" ng-class="col.colIndex()" style="border:2px solid white;height:40px">{{row.getProperty(col.field)}}</div>';
         var issuecellTemplate = 
