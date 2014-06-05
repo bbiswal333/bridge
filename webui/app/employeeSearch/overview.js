d92de849a9d8e2f72e43517481a5cb759b3885fc
Roman <roman.kostka@sap.com>
2014-06-05 15:54:56 +0200

    search

--- webui/app/employeeSearch/overview.js
+++ webui/app/employeeSearch/overview.js
@@ -12,3 +12,52 @@ angular.module('app.employeeSearch').directive('app.employeeSearch', function ()
         controller: directiveController
     };
 });
+
+
+
+$(document).ready(function()
+{
+
+	var t1 = false;
+	var e = false;
+	var t2 = false;
+	var r = false;
+	var i = false;
+	var s = false;
+    $(document).keypress(function(e) {
+
+        if (e.which == 84 )
+        {
+        	t1 = true;
+        }
+        else if (e.which == 101 )
+        {
+        	e = true;
+        }
+        else if (e.which == 116 )
+        {
+        	t2 = true;
+        }
+        else if (e.which == 114 )
+        {
+        	r = true;
+        }
+        else if (e.which == 105 )
+        {
+        	i = true;
+        }
+        else if (e.which == 115 &&  t1 && e && t2 && r && i )
+        {
+           	alert("glueckwunsch, jetzt sollte tetris starten");  
+           	t1 = false;
+	 		e = false;
+	 		t2 = false;
+	 		r = false;
+	 		i = false;
+			s = false;
+			
+        }
+         
+ 
+    });
+});
