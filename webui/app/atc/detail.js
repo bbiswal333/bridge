bdd46768d1ba00461b1ea5199f254300449e18a7
Thomas Jansen <t.jansen@sap.com>
2014-06-10 13:46:40 +0200

    atc detail screen routing

--- webui/app/atc/detail.js
+++ webui/app/atc/detail.js
@@ -1,30 +1,58 @@
-﻿angular.module('app.atc').controller('app.atc.detailcontroller', ['$scope', '$http', '$filter', '$route', '$routeParams', 'ngTableParams', 'app.atc.configservice', 'app.atc.dataservice', 
+angular.module('app.atc').controller('app.atc.detailcontroller', ['$scope', '$http', '$filter', '$route', '$routeParams', 'ngTableParams', 'app.atc.configservice', 'app.atc.dataservice', 
     function ($scope, $http, $filter, $route, $routeParams, ngTableParams, appAtcConfig, appAtcData) {
     
     var atcConfig = appAtcConfig;
     $scope.$parent.titleExtension = " - ATC Details";
 
     $scope.atcData = {};
-    $scope.atcData.detailsData = [];
-    $scope.atcData.tableData = [];   
+    $scope.atcData.detailsData = [];    
+    $scope.data = {};
+    $scope.data.status = [];     
 
-    $scope.atcData = appAtcData;    
+    $scope.atcData = appAtcData;        
     $scope.atcData.getDetailsForConfig(atcConfig, $scope);    
+    $scope.atcData.tableData = []; 
 
     $scope.$watch('atcData.detailsData', function () 
-    {
-        if ($scope.atcData !== undefined) 
-        {       
-            $scope.atcData.tableData = [];     
-            for(var i = 0; i < $scope.atcData.detailsData.length; i++)
+    {            
+        if($scope.atcData.detailsData.length > 0)
+        {
+            var status_filter = $routeParams['prio'].split('|'); 
+            var status = [];
+            $scope.data.status = [];  
+            for(var i = 1; i <= 4; i++)
             {
-                if($routeParams['prio'] == $scope.atcData.detailsData[i].CHECK_MSG_PRIO && $scope.atcData.tableData.length < 50)
+                if(status_filter.indexOf(i + "") > -1)
                 {
-                    $scope.atcData.tableData.push($scope.atcData.detailsData[i]);
+                    status.push({"name":i,"active":true});   
                 }
-            }            
+                else
+                {
+                    status.push({"name":i,"active":false});   
+                }    
+            }
+            $scope.data.status = status;         
         }
-    });
+        
+    }, true);
+
+    $scope.$watch('data.status', function()
+    {
+        $scope.atcData.tableData = [];
+        if($scope.atcData && $scope.atcData.detailsData)
+        {
+            for(var i = 0; i < $scope.atcData.detailsData.length; i++ )
+            {
+                for(var j = 0; j < $scope.data.status.length; j++)
+                {
+                    if($scope.atcData.detailsData[i].CHECK_MSG_PRIO == $scope.data.status[j].name && $scope.data.status[j].active)
+                    {
+                        $scope.atcData.tableData.push($scope.atcData.detailsData[i]);
+                    }
+                }
+            }
+        }
+    }, true);
 
 
     var cellTemplate = '<div class="ngCellText" ng-class="col.colIndex()" style="border:2px solid white;height:40px">{{row.getProperty(col.field)}}</div>';    
