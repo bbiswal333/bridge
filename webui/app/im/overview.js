368666b42273e2f1c3e84c168c75829700ad08ab
Thomas Jansen <t.jansen@sap.com>
2014-06-23 14:12:32 +0200

    internal message screen selection (only UI)

--- webui/app/im/overview.js
+++ webui/app/im/overview.js
@@ -2,7 +2,7 @@ angular.module('app.im', ['ngTable']);
 
 angular.module('app.im').directive('app.im', function () {
 
-    var directiveController = ['$scope' ,'app.im.configservice', function ($scope, appimconfigservice) {  
+    var directiveController = ['$scope', function ($scope) {  
         $scope.box.boxSize = "1";      
     }];
 
@@ -13,59 +13,58 @@ angular.module('app.im').directive('app.im', function () {
     };
 });
 
-    angular.module('app.im').run(function ($rootScope) {
-
-        //weatherMAGIC
-
-            function get_location() {
-                console.log('get_Location start');
-              if (Modernizr.geolocation) {
-                navigator.geolocation.getCurrentPosition(show_map);
-              } else {
-                // no native support; maybe try a fallback?
-              }
-            }
-        
-});
-
-angular.module('app.im').controller('app.im.directiveController', ['$scope', '$http', 'app.im.ticketData',
+ angular.module('app.im').controller('app.im.directiveController', ['$scope', '$http', 'app.im.ticketData',
     function Controller($scope, $http, ticketData) {
 
         $scope.prios = [{
-            name: "Very high", number: 1, amount: 0,
+            name: "Very high", number: 1, amount: 0
         }, {
-            name: "High", number: 2, amount: 0,
+            name: "High", number: 2, amount: 0
         }, {
-            name: "Medium", number: 3, amount: 0,
+            name: "Medium", number: 3, amount: 0
         }, {
-            name: "Low", number: 4, amount: 0,
+            name: "Low", number: 4, amount: 0
         }];
 
+        $scope.selection = {};
+        $scope.selection.sel_components = true;
+        $scope.selection.colleagues = false;
+        $scope.selection.assigned_me = false;
+        $scope.selection.created_me = false;
+
+
         function parseBackendTicket(backendTicket) {
-            _.each($scope.prios, function (prios) {
-                if (backendTicket.PRIO == prios.number.toString())
+
+            angular.forEach($scope.prios, function(prios) 
+            {
+                if (backendTicket.PRIO === prios.number.toString())
+                {
                     prios.amount++;
+                }
             });
+
         }
 
         $scope.$parent.titleExtension = " - Internal Messages"; 
         $http.get('https://css.wdf.sap.corp:443/sap/bc/devdb/MYINTERNALMESS?sap-language=en&origin=' + location.origin
             ).success(function(data) {
                 data = new X2JS().xml_str2json(data);
-                var imData = data["abap"];
-                ticketData.backendTickets = imData["values"];
+                var imData = data.abap;
+                ticketData.backendTickets = imData.values;
 
                 // if you have multiple tickets, DEVDB_MESSAGE_OUT is an array, otherwise a simple object
-                if (angular.isArray(ticketData.backendTickets.INTCOMP_LONG.DEVDB_MESSAGE_OUT)) {
-                    _.each(ticketData.backendTickets.INTCOMP_LONG.DEVDB_MESSAGE_OUT, function (backendTicket) {
+                if (angular.isArray(ticketData.backendTickets.INTCOMP_LONG.DEVDB_MESSAGE_OUT)) 
+                {
+                    angular.forEach(ticketData.backendTickets.INTCOMP_LONG.DEVDB_MESSAGE_OUT, function (backendTicket) 
+                    {
                         parseBackendTicket(backendTicket);
                     });
                 } else if (angular.isObject(ticketData.backendTickets.INTCOMP_LONG.DEVDB_MESSAGE_OUT)) {
                     parseBackendTicket(ticketData.backendTickets.INTCOMP_LONG.DEVDB_MESSAGE_OUT);
                 }
 
-                if (($scope.prios[0].amount + $scope.prios[1].amount + $scope.prios[2].amount + $scope.prios[3].amount) == 0) {
-                    $scope.lastElement="You have no internal messages to display!";
+                if (($scope.prios[0].amount + $scope.prios[1].amount + $scope.prios[2].amount + $scope.prios[3].amount) === 0) {
+                    $scope.lastElement = "You have no internal messages to display!";
                     $scope.displayChart = false;
                 }                
                 else 
@@ -73,7 +72,7 @@ angular.module('app.im').controller('app.im.directiveController', ['$scope', '$h
                     $scope.displayChart = true;
                 }
 
-            }).error(function(data) {
+            }).error(function() {
                 $scope.imData = [];                
             });
 
