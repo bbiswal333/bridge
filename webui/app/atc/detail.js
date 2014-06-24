53e4f5e8cb507a516887685f1fc3ff36bd2b2e39
Kharitonov, Anton <anton.kharitonov@sap.com>
2014-06-24 16:28:18 +0200

    clearing ATC detals.js

--- webui/app/atc/detail.js
+++ webui/app/atc/detail.js
@@ -2,14 +2,16 @@ angular.module('app.atc').controller('app.atc.detailcontroller', ['$scope', '$ht
     function ($scope, $http, $filter, $route, $routeParams, ngTableParams, appAtcConfig, appAtcData) {
     
     $scope.$parent.titleExtension = " - ATC Details";
+    $scope.filterText = '';
+
 
     $scope.atcData = {};
     $scope.atcData.detailsData = [];    
-    $scope.data = {};
-    $scope.data.status = [];     
 
     $scope.atcData = appAtcData;          
     $scope.atcData.tableData = [];
+    
+    $scope.statusArray = [];     
 
     var infinityLimitStep = 100;
     $scope.infinityLimit = infinityLimitStep;
@@ -29,33 +31,29 @@ angular.module('app.atc').controller('app.atc.detailcontroller', ['$scope', '$ht
         {
             var status_filter = $routeParams['prio'].split('|'); 
             var status = [];
-            $scope.data.status = [];  
+            $scope.statusArray = [];  
             for(var i = 1; i <= 4; i++)
             {
                 if(status_filter.indexOf(i + "") > -1)
-                {
                     status.push({"name":i,"active":true});   
-                }
                 else
-                {
                     status.push({"name":i,"active":false});   
-                }    
             }
-            $scope.data.status = status;         
+            $scope.statusArray = status;         
         }
         
     }, true);
 
-    $scope.$watch('data.status', function()
+    $scope.$watch('statusArray', function()
     {
         $scope.atcData.tableData = [];
         if($scope.atcData && $scope.atcData.detailsData)
         {
             for(var i = 0; i < $scope.atcData.detailsData.length; i++ )
             {
-                for(var j = 0; j < $scope.data.status.length; j++)
+                for(var j = 0; j < $scope.statusArray.length; j++)
                 {
-                    if($scope.atcData.detailsData[i].CHECK_MSG_PRIO == $scope.data.status[j].name && $scope.data.status[j].active)
+                    if($scope.atcData.detailsData[i].CHECK_MSG_PRIO == $scope.statusArray[j].name && $scope.statusArray[j].active)
                     {
                         $scope.atcData.tableData.push($scope.atcData.detailsData[i]);
                     }
@@ -79,25 +77,22 @@ angular.module('app.atc').controller('app.atc.detailcontroller', ['$scope', '$ht
 
     var cellTemplate = '<div class="ngCellText table-cell" ng-class="col.colIndex()" >{{row.getProperty(col.field)}}</div>';    
 
-    $scope.filterOptions = {
-        filterText: ''
-    };
 
-    $scope.gridOptions = {                        
-        enableColumnReordering:true,
-        enableRowSelection:false,            
-        rowHeight: 40,
-        showFilter:false,
-        filterOptions: $scope.filterOptions,
-        columnDefs: [
-            {field:'CHECK_MSG_PRIO', displayName:'Prio', width:'20%', cellTemplate: cellTemplate},                
-            {field:'CHECK_SYSTEM', displayName:'System', width:'20%', cellTemplate: cellTemplate},
-            {field:'CHECK_DESCRIPTION', displayName:'Check', width:'20%', cellTemplate: cellTemplate},
-            {field:'CHECK_MESSAGE', displayName:'Check Message', width:'20%', cellTemplate: cellTemplate},
-            {field:'OBJ_NAME', displayName:'Object', width:'20%', cellTemplate: cellTemplate},
-        ],
-        plugins: [new ngGridFlexibleHeightPlugin()]
-    }       
+    // $scope.gridOptions = {                        
+    //     enableColumnReordering:true,
+    //     enableRowSelection:false,            
+    //     rowHeight: 40,
+    //     showFilter:false,
+    //     filterOptions: $scope.filterOptions,
+    //     columnDefs: [
+    //         {field:'CHECK_MSG_PRIO', displayName:'Prio', width:'20%', cellTemplate: cellTemplate},                
+    //         {field:'CHECK_SYSTEM', displayName:'System', width:'20%', cellTemplate: cellTemplate},
+    //         {field:'CHECK_DESCRIPTION', displayName:'Check', width:'20%', cellTemplate: cellTemplate},
+    //         {field:'CHECK_MESSAGE', displayName:'Check Message', width:'20%', cellTemplate: cellTemplate},
+    //         {field:'OBJ_NAME', displayName:'Object', width:'20%', cellTemplate: cellTemplate},
+    //     ],
+    //     plugins: [new ngGridFlexibleHeightPlugin()]
+    // }       
 }]);
 
 angular.module('app.atc').directive("infinitescroll", ['$window', function($window){
@@ -106,7 +101,7 @@ angular.module('app.atc').directive("infinitescroll", ['$window', function($wind
         var cont = container[0];
 
         container.bind("scroll", function() {
-            console.log(cont.scrollTop, cont.offsetHeight, elm[0].scrollHeight);
+            //console.log(cont.scrollTop, cont.offsetHeight, elm[0].scrollHeight);
             if (cont.scrollTop + cont.offsetHeight >= elm[0].scrollHeight + elm[0].offsetTop) {
                 scope.$apply(scope.increaseInfinityLimit());
             } else if (cont.scrollTop === 0){
