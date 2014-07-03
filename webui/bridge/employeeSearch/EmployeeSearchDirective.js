2817eb1d0c7fcfd56c23bf83a7a04f16036f546e
Thomas Jansen <t.jansen@sap.com>
2014-07-03 08:14:57 +0200

    google maps link and link to costcenter for employee search

--- webui/bridge/employeeSearch/EmployeeSearchDirective.js
+++ webui/bridge/employeeSearch/EmployeeSearchDirective.js
@@ -18,7 +18,20 @@ angular.module('bridge.employeeSearch').directive('bridge.employeeSearch', funct
             $http.get('https://ifp.wdf.sap.corp:443/sap/bc/zxa/FIND_EMPLOYEE_JSON?id=' + $item.BNAME + '&origin=' + location.origin).then(function (response) {
                 $scope.selectedEmployee = response.data.DATA;
                 $scope.selectedEmployee.TELNR = $scope.selectedEmployee.TELNR_DEF.replace(/ /g, '').replace(/-/g, '');                    
-            });
+
+                $http.get('/bridge/employeeSearch/buildings.xml').then(function (response) {
+                //console.log(response.data);
+                var data = new X2JS().xml_str2json(response.data);            
+                for(var i = 0; i < data.items.item.length; i++)
+                {
+                    if(data.items.item[i].objidshort === $item.BUILDING && data.items.item[i].geolinkB !== undefined)
+                    {
+                        $scope.selectedEmployee.building_url = data.items.item[i].geolinkB;
+                    }                    
+                }
+                });
+
+            });    
         };
     }];
 
