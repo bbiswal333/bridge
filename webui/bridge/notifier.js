ff238d94ff1d96bd59b8d4a10a7c3302d5558eb2
D051804 <daniel.schueler@sap.com>
2014-07-03 13:44:34 +0200

    notifier.js now eslint compatible

--- webui/bridge/notifier.js
+++ webui/bridge/notifier.js
@@ -5,12 +5,6 @@ angular.module("notifier", []).factory("notifier", function () {
   icons.push("../img/notifier_red_cross.png"); // Error
   var DEFAULT_DURATION = 5000;
 
-  function showMsg(title_s, body_s, icon_i, tag_s, onCLick_fn) {
-      var notifier = new Notifier(title_s, body_s, icons[icon_i], tag_s, DEFAULT_DURATION);
-      notifier.onclick = onCLick_fn;
-      notifier.show();
-  }
-
   var Notifier = function (text, body, icon, tag, duration) {
     var self = this;
     var n;
@@ -26,6 +20,33 @@ angular.module("notifier", []).factory("notifier", function () {
     this.onclose = undefined;
     this.onerror = undefined;
     this.permissionCallback = undefined;
+
+    function checkPermission(callbackGranted_fn, callbackDenied_fn, callbackNoSupport_fn, callbackPermissionRequest_fn) {
+        if (typeof window.Notification === "undefined") {
+            callbackNoSupport_fn();
+        }
+        else if (Notification.permission === "granted") {
+            callbackGranted_fn();
+        }
+        else if (Notification.permission === "default") {
+            console.log("seems like you need to activate once");
+            callbackPermissionRequest_fn();
+        }
+        else if (Notification.permission !== "denied") {
+            Notification.requestPermission(function (perm) {
+                if (perm === "granted") {
+                    callbackGranted_fn();
+                }
+                else {
+                    callbackDenied_fn();
+                }
+            });
+        }
+        else {
+            callbackDenied_fn();
+        }
+    }
+
     this.onNotificationsNotSupported = function () {
       alert("Your browser does not support the Notification API");
     };
@@ -41,29 +62,39 @@ angular.module("notifier", []).factory("notifier", function () {
           tag: self.tag
         });
         n.onshow = function () {
-          if (typeof self.onshow != "undefined") self.onshow(n.tag);
-          if (duration >= 0) {
-            setTimeout(function () {
-              self.close();
-            }, self.duration);
-          }
+            if (typeof self.onshow !== "undefined") {
+                self.onshow(n.tag);
+            }
+            if (duration >= 0) {
+                setTimeout(function () {
+                    self.close();
+                }, self.duration);
+            }
         };
         n.onclick = function () {
-          if (typeof self.onclick != "undefined") self.onclick(n.tag);
+            if (typeof self.onclick !== "undefined") {
+                self.onclick(n.tag);
+            }
         };
         n.onclose = function () {
-          if (typeof self.onclose != "undefined") self.onclose(n.tag);
+            if (typeof self.onclose !== "undefined") {
+                self.onclose(n.tag);
+            }
         };
         n.onerror = function () {
-          if (typeof self.onerror != "undefined") self.onerror(n.tag);
+            if (typeof self.onerror !== "undefined") {
+                self.onerror(n.tag);
+            }
         };
       }, self.onNotificationsDenied, self.onNotificationsNotSupported, function () {
-        if (typeof self.permissionCallback != "undefined") self.permissionCallback();
+          if (typeof self.permissionCallback !== "undefined") {
+              self.permissionCallback();
+          }
       });
     };
 
     this.close = function () {
-      if (typeof n != "undefined") {
+      if (typeof n !== "undefined") {
         n.close();
       }
     };
@@ -73,7 +104,7 @@ angular.module("notifier", []).factory("notifier", function () {
     };
 
     this.getPermission = function () {
-     if (typeof window.Notification == "undefined") { 
+     if (typeof window.Notification === "undefined") { 
         return undefined;
       }
       else if (Notification.permission === "granted") {
@@ -86,32 +117,6 @@ angular.module("notifier", []).factory("notifier", function () {
         return 'pleaseAsk';
       }
     };
-
-    function checkPermission(callbackGranted_fn, callbackDenied_fn, callbackNoSupport_fn, callbackPermissionRequest_fn) {
-      if (typeof window.Notification == "undefined") { 
-        callbackNoSupport_fn();
-      }
-      else if (Notification.permission === "granted") {
-        callbackGranted_fn();
-      }
-      else if (Notification.permission === "default") {
-        console.log("seems like you need to activate once");
-        callbackPermissionRequest_fn();
-      }
-      else if (Notification.permission !== "denied") {
-        Notification.requestPermission(function (perm) {
-          if (perm === "granted") {
-            callbackGranted_fn();
-          }
-          else {
-            callbackDenied_fn();
-          }
-        });
-      }
-      else {
-        callbackDenied_fn();
-      }
-    }
   };
 
   //Because Chromium has a quite special implementation (Explicit user action is required for calling Notification.requestPermission()),
@@ -119,17 +124,23 @@ angular.module("notifier", []).factory("notifier", function () {
   //we need to display a screen where the user can explicitly take an action (press a button etc.) calling the requestPermission()-Method
   //Also see here: http://stackoverflow.com/questions/5040107/webkit-notifications-requestpermission-function-doesnt-work
   Notifier.prototype.chromePreCheckRequestNeeded = function () {
-    if (typeof window.Notification != "undefined") {
+    if (typeof window.Notification !== "undefined") {
       return (Notification.permission === "default");  
     }
     return false;
   };
 
   Notifier.prototype.requestPermission = function (callback_fn) {
-    if (typeof window.Notification != "undefined") {
+    if (typeof window.Notification !== "undefined") {
       Notification.requestPermission(callback_fn);
     }
   };
+
+  function showMsg(title_s, body_s, icon_i, tag_s, onCLick_fn) {
+      var notifier = new Notifier(title_s, body_s, icons[icon_i], tag_s, DEFAULT_DURATION);
+      notifier.onclick = onCLick_fn;
+      notifier.show();
+  }
 
   var instance = new Notifier();
 
@@ -147,7 +158,7 @@ angular.module("notifier", []).factory("notifier", function () {
       return instance.chromePreCheckRequestNeeded();
     },
     requestPermission: function (callback_fn) {
-      instance.requestPermission(function (perm) {
+      instance.requestPermission(function () {
         callback_fn();
       });
     },
