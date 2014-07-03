3bfa640abc2838a94e2be7b60761b6836bd5f7e1
riccardb <riccardo.biasibetti@sap.com>
2014-07-03 16:03:00 +0200

    allocation bars blue color coding

--- webui/app/cats/detail/allocationBar/allocationBarUtils.js
+++ webui/app/cats/detail/allocationBar/allocationBarUtils.js
@@ -2,11 +2,22 @@ angular.module("app.cats.allocationBar.utils", []).service("app.cats.allocationB
     this.colorCounter = 0;
 
     this.colors = [
-        "#18334a",
-        "#2d5f8a",
-        "#428bca",
-        "#3a79b0",
-        "#4694d7"
+           "#2574A9",
+           "#5C97BF",
+           "#6BB9F0",
+           "#52B3D9",
+           "#22A7F0",
+           "#3498db",
+           "#4b77be",
+           "#89c4f4",
+           "#3a539b",
+           "#67809f",
+
+        // "#18334a",
+        // "#2d5f8a",
+        // "#428bca",
+        // "#3a79b0",
+        // "#4694d7"
     ];
     this.blockColors = {};
 
