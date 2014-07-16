0a38f0578d08a8cd68f72dda3f1ddfa9bbab72ef
Rene Laengert <rene.laengert@sap.com>
2014-07-16 22:52:57 +0200

    meetings box size configurable

--- webui/app/lunchWalldorf/settings.js
+++ webui/app/lunchWalldorf/settings.js
@@ -1,4 +1,5 @@
-angular.module('app.lunchWalldorf').applunchWalldorfSettings = ['$scope', "app.lunchWalldorf.configservice", function ($scope, lunchConfigService) {    
+angular.module('app.lunchWalldorf').applunchWalldorfSettings =
+['$scope', "app.lunchWalldorf.configservice", function ($scope, lunchConfigService) {    
 	$scope.currentConfigValues = lunchConfigService.configItem;
 
     $scope.save_click = function () {  
