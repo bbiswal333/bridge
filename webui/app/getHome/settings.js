f775b89f05bfd134928b66ae66ca250f9868d75d
d051820 <matthias.gradl@sap.com>
2014-07-18 16:33:35 +0200

    Add typeahead search for address

--- webui/app/getHome/settings.js
+++ webui/app/getHome/settings.js
@@ -1,6 +1,6 @@
 angular.module('app.getHome').appGetHomeSettings = 
-	['app.getHome.configservice', '$scope',  
-		function (appGetHomeConfig, $scope) {
+	['app.getHome.configservice', 'app.getHome.mapservice', '$scope', '$q', 
+		function (appGetHomeConfig, appGetHomeMap, $scope, $q) {
 
 	$scope.config  = appGetHomeConfig;
 
@@ -11,11 +11,15 @@ angular.module('app.getHome').appGetHomeSettings =
 
 	$scope.addNewLocation = function() {
 		var location = {
-			name: $scope.newLocation.name
+			name: $scope.newLocation.name,
+			address: $scope.newLocation.address.address.label,
+			latitude: $scope.newLocation.address.displayPosition.latitude, 
+			longitude: $scope.newLocation.address.displayPosition.longitude
 		};
 
 		appGetHomeConfig.data.locations.push(location);
 		$scope.newLocation.name = '';
+		$scope.newLocation.address = '';
 		$scope.addMode = false;
 	};
 
@@ -23,8 +27,22 @@ angular.module('app.getHome').appGetHomeSettings =
 		appGetHomeConfig.data.locations.splice(index, 1);
 	};
 
+	$scope.searchAddress = function(searchString) {
+		console.log(searchString);
+		var addresses = $q.defer();
+		appGetHomeMap.search(searchString, function(locations) {
+			console.log(locations);
+			addresses.resolve(locations);
+		})
+		addresses.promise.then(function(data) {
+			return data;
+		});
+		return addresses.promise;
+	};
+
 	$scope.closeForm = function () {
 		$scope.$emit('closeSettingsScreen');
 	};
 
 }];
+
