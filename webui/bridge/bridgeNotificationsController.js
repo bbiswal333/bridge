87487ceaa9b37c6f2e0cfe8644277a4e18ba1f2e
Sebastian Woinar <s.woinar@sap.com>
2014-07-18 14:34:51 +0200

    Add callback for notification sidebar seen

--- webui/bridge/bridgeNotificationsController.js
+++ webui/bridge/bridgeNotificationsController.js
@@ -1,63 +1,56 @@
 angular.module('bridge.app').
 	controller('notificationsController',['$rootScope', '$scope', '$filter', '$timeout', 'bridgeConfig','bridgeDataService', "notifier",
 	function ($rootScope, $scope, $filter, $timeout, bridgeConfig, bridgeDataService, notifier){
-	    $scope.bridgeSettings = bridgeDataService.getBridgeSettings();
-        $scope.dummyData = ["hello", "testA"];
-	    //$scope.apps = bridgeDataService.getProjects()[0].apps;
+
+    //$scope.apps = bridgeDataService.getProjects()[0].apps;
 
 		$scope.notifications = notifier.allNotifications();
 
-			//$scope.newNotifications = $filter('newNotification')(notifier.allNotifications());
 
-    	$scope.notificationSupported = notifier.getPermission();
 
-    	$scope.clearNotifications = function() {
-    		notifier.clearNotifications();
-    		return false;
-    	}
+		$scope.onShowNotifications = function(){
+				// toDo: set all notifications to state: seen
+		};
 
-    	function areNotificationsSupported() {
-            $timeout(function () {
-                $scope.notificationSupported = notifier.getPermission();
-            }, 500);
-        }
-
-      $scope.requestPermission = function(){
-      	notifier.requestPermission( areNotificationsSupported );
-      };
+  	$scope.clearNotifications = function() {
+  		notifier.clearNotifications();
+  		return false;
+  	}
 
 	  $scope.testNotification = function(){
 	  	notifier.showSuccess("Test","Notification is working","Settings");
 
 	  };
 
-	$scope.retrieve_xkdc_entry = function(){
-		$.ajax({
-				url: "https://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
-				dataType: "json",
-				jsonpCallback: "xkcddata",
-				success: function(data) {
-						$("#xkcdcontent").append(
-								$("<p>Just read the latest XKCD</p>"),
-								$("<img/>").attr({
-										src: data.img,
-										title: data.alt,
-										alt: data.title,
-										style: 'width:80%'
-								}),
-								$("<p><a href='http://xkcd.com'>xkcd</a></p>")
-						);
-				}
-		});
-	};
 
-			$scope.amountOfNewNotifications = function(){
-				return $filter('filter')(notifier.allNotifications(), {state:'new'}).length;
-			};
+		$scope.retrieve_xkdc_entry = function(){
+			$.ajax({
+					url: "https://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
+					dataType: "json",
+					jsonpCallback: "xkcddata",
+					success: function(data) {
+							$("#xkcdcontent").append(
+									$("<p>Just read the latest XKCD</p>"),
+									$("<img/>").attr({
+											src: data.img,
+											title: data.alt,
+											alt: data.title,
+											style: 'width:80%'
+									}),
+									$("<p><a href='http://xkcd.com'>xkcd</a></p>")
+							);
+					}
+			});
+		};
 
-			$scope.filterNewNotifications = function newNotifications(item) {
-				return item.state == "new";
-			};
+		$scope.amountOfNewNotifications = function(){
+			return $filter('filter')(notifier.allNotifications(), {state:'new'}).length;
+		};
+
+		$scope.filterNewNotifications = function newNotifications(item) {
+			return item.state == "new";
+		};
+		
 		}]).
 		filter('newNotification', function () {
 				return function (notifications) {
