99d2ed4c5c8ccf49cf35811b63e38eb942a0b490
D054485 <m.beutlberger@sap.com>
2014-07-18 10:49:29 +0200

    Added nokia API
    Loading of and authenticating with nokia API

--- webui/app/getHome/overview.js
+++ webui/app/getHome/overview.js
@@ -4,6 +4,10 @@ angular.module('app.gethome').directive('app.gethome', function () {
     var directiveController = ['$scope', function ($scope)
     {
         //put some stuff in here
+
+        nokia.Settings.set("app_id", "BGFtzY6olMoTQcTu9MGp");
+		nokia.Settings.set("app_code", "pbI1l9jZBzUsw0pouKowHA");
+
     }];
 
     return {
