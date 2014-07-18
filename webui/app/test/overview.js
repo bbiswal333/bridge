5cb7c2098773b59084997177b617917edb306371
Sebastian Woinar <s.woinar@sap.com>
2014-07-18 16:27:36 +0200

    remove stub again

--- webui/app/test/overview.js
+++ webui/app/test/overview.js
@@ -8,27 +8,17 @@ angular.module('app.test').directive('app.test', function () {
             $scope.results = 'Running...';
             setTimeout(function(){
                 if (!(Math.random()+.5|0)) {
-                    notifier.showSuccess("Test Results: ", 
-                                    "1 of 143 passed.", 
-                                    $scope.$parent.module_name, 
+                    notifier.showSuccess("Test Results: ",
+                                    "1 of 143 passed.",
+                                    $scope.$parent.module_name,
                                     function() {alert('Congratulations!')});
                     $scope.results = '1 tests passed.';
-                    notifier.showError("Test : ", 
-                                        "2 of 143 failed.", 
-                                        $scope.$parent.module_name, 
-                                        function() {alert(':--(')});
-
-                    notifier.showError("aasd : ", 
-                                        "adesc.", 
-                                        "aasd", 
-                                        function() {alert(':--(')});
-                    $scope.results = '2 tests passed.';
                 } else {
-                    notifier.showError("Test Results: ", 
-                                        "10 of 143 failed.", 
-                                        $scope.$parent.module_name, 
+                    notifier.showError("Test Results: ",
+                                        "10 of 143 failed.",
+                                        $scope.$parent.module_name,
                                         function() {alert(':-(')});
-                    $scope.results = '10 tests failed.';    
+                    $scope.results = '10 tests failed.';
                 };
             }, 3000);
         };
