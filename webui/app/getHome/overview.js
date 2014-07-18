53017b945d7aee57c1dc29005cd21590a2b13879
Matthias Osswald <mat.osswald@sap.com>
2014-07-18 10:21:32 +0200

    added initial get-home app

---
+++ webui/app/getHome/overview.js
@@ -0,0 +1,14 @@
+﻿angular.module('app.gethome', []);
+angular.module('app.gethome').directive('app.gethome', function () {
+
+    var directiveController = ['$scope', function ($scope)
+    {
+        //put some stuff in here
+    }];
+
+    return {
+        restrict: 'E',
+        templateUrl: 'app/getHome/overview.html',
+        controller: directiveController
+    };
+});
