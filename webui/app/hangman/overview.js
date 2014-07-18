bdc5e42e531547083273ef95b142f30e03616f72
d056949 <obatzer@gmail.com>
2014-07-18 16:54:29 +0200

    done

---
+++ webui/app/hangman/overview.js
@@ -0,0 +1,15 @@
+﻿angular.module('app.hangman', []);
+angular.module('app.hangman').directive('app.hangman', function () {
+
+    var directiveController = ['$scope', 'notifier', function ($scope, notifier)
+    {
+        $scope.numNotifications = notifier.allNotifications().length;
+        
+    }];
+
+    return {
+        restrict: 'E',
+        templateUrl: 'app/hangman/overview.html',
+        controller: directiveController
+    };
+});
