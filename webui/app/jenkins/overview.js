936c0797a4dde78726aa0384a0847df800d8bfd5
Richard Steuer <richard.steuer@sap.com>
2014-08-05 15:20:28 +0200

    Removed "Trigger build" button; added credentials flag to enable CORS in frontend

--- webui/app/jenkins/overview.js
+++ webui/app/jenkins/overview.js
@@ -16,35 +16,22 @@ angular.module('app.jenkins').directive('app.jenkins', function () {
 
             $scope.jenkinsConfig.url = url;
 
-            $http({method: 'GET', url: url + "/api/json"}).
-                success(function(data) {
+            $http.get(url + "/api/json", {withCredentials: false})
+                 .success(function (data) {
                     $scope.jobs = data.jobs;
                     addBuildStatusUrls();
-                }).
-                error(function(data, status) {
+                }).error(function(data, status) {
                     console.log("Error retrieving " + url + ". Status: " + status);
                     $scope.jobs = [];
                 });
+
         };
 
         var init = function() {
-            updateJenkins("http://mo-e7882d540.mo.sap.corp:49153");
+            updateJenkins("http://mo-c97a0800b.mo.sap.corp:49153");
         };
 
         init();
-
-        $scope.triggerBuild = function(job) {
-
-            var buildUrl = $scope.jenkinsConfig.url + "/job/" + job.name + "/build";
-
-            $http({method: 'POST', url: buildUrl}).
-                success(function(data, status, headers, config) {
-                    console.log("Success! Data: " + data);
-                }).
-                error(function(data, status) {
-                    console.log("Error posting to trigger build. Status: " + status + ", data: " + data);
-                });
-        };
 
         $scope.inputChanged = function() {
             updateJenkins($scope.jenkinsConfig.url);
