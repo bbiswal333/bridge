034ddcbecae2855180a8482f71ebd1d14a211a92
D051804 <daniel.schueler@sap.com>
2014-08-05 16:09:17 +0200

    missing files from last commit

---
+++ webui/bridge/bridgeModuleSetup.spec.js
@@ -0,0 +1,51 @@
+﻿describe("The bridge config", function () {
+    var $route;
+    var $rootScope;
+    var $q;
+    var $log;
+    var bridgeDataService;
+
+    beforeEach(function () {
+        module("bridge.app");
+        module("bridge.service");
+
+        sessionStorage.clear();
+
+        inject(["$route", "$rootScope", "$q", "$log", "bridgeDataService", function (_$route, _$rootScope, _$q, _$log, _bridgeDataService) {
+            $route = _$route;
+            $rootScope = _$rootScope;
+            $q = _$q;
+            $log = _$log;
+            bridgeDataService = _bridgeDataService;
+        }]);
+    });
+
+    it("should register routes for detail pages", function () {
+        expect($route.routes['/detail/atc/:appId/:prio'].templateUrl).toBe('view/detail.html'); // navigate to the general detail screen
+    });
+
+    it("should pass the correct data to the detail pages", function () {
+        var routeMock = {
+            current: {
+                app: $route.routes['/detail/atc/:appId/:prio'].app,
+                info: $route.routes['/detail/atc/:appId/:prio'].info
+            }
+        };
+
+
+        var promise = $route.routes['/detail/atc/:appId/:prio'].resolve.appInfo($q, routeMock);
+        promise.then(function success(data) {
+            expect(data.module_name).toBe("app.atc");
+            expect(data.needs_client).toBe(false);
+        }, function error() {
+            expect("Promise").toBe("rejected");
+        });
+    });
+
+    it("should extend the angular $log service to log to the session storage", function () {
+        bridgeDataService.setLogMode(true);
+        $log.log("Test log message");
+        var log = JSON.parse(sessionStorage.getItem("bridgeLog"));
+        expect(log[0].sMessage).toBe("Test log message");
+    });
+});
