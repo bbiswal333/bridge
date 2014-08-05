5a0c3cc838edcf88d3e721030c3303415be91b67
Rene Laengert <rene.laengert@sap.com>
2014-08-05 15:04:18 +0200

    Fixed EWS unit tests

--- webui/app/meetings/nextEventBoxDirective.spec.js
+++ webui/app/meetings/nextEventBoxDirective.spec.js
@@ -30,8 +30,8 @@ describe("nextEventBoxDirective ewsHelperUtils test", function () {
 
 	it("should be tested by a working test :-)", function () {
 		//3 days from 30.01.2014 10:15:30
-		var url = window.client.origin + "localhost:8000/api/getCalDataSSO?from=" + encodeForUrl("2014-01-30T10:15:05Z") + "&to=" + encodeForUrl("2014-02-02T10:15:05Z") + "&format=json";
-		var expectation = new RegExp(".*\?from=" + encodeForUrl("2014-01-30T10:15:05Z") + "&to=" + encodeForUrl("2014-02-02T10:15:05Z") + "&format=json$");
+		var url = window.client.origin + "localhost:8000/api/getCalDataSSO?dateFrom=" + encodeForUrl("2014-01-30T10:15:05Z") + "&dateTo=" + encodeForUrl("2014-02-02T10:15:05Z") + "&format=json";
+		var expectation = new RegExp(".*\?dateFrom=" + encodeForUrl("2014-01-30T10:15:05Z") + "&dateTo=" + encodeForUrl("2014-02-02T10:15:05Z") + "&format=json$");
 
 		expect(url).toMatch(expectation);
 	});
@@ -39,7 +39,7 @@ describe("nextEventBoxDirective ewsHelperUtils test", function () {
 	it("should build a correct url", function () {
 		//3 days from 30.01.2014 10:15:30
 		var url = ewsHelperUtils.buildEWSUrl(new Date(2014, 0, 30, 10, 15, 5), 3); //0 for January, JavaScript is tricky here!
-		var expectation = new RegExp(".*\?from=" + encodeForUrl("2014-01-30T10:15:05Z") + "&to=" + encodeForUrl("2014-02-02T10:15:05Z") + "&format=json$");
+		var expectation = new RegExp(".*\?dateFrom=" + encodeForUrl("2014-01-30T10:15:05Z") + "&dateTo=" + encodeForUrl("2014-02-02T10:15:05Z") + "&format=json$");
 
 		expect(url).toMatch(expectation);
 	});
@@ -47,7 +47,7 @@ describe("nextEventBoxDirective ewsHelperUtils test", function () {
 	it("should oncemore build a correct url", function () {
 		//1 day from 28.02.2014 00:00:00
 		var url = ewsHelperUtils.buildEWSUrl(new Date(2014, 1, 28, 0, 0, 0), 1); //1 for February, JavaScript is tricky here!
-		var expectation = new RegExp(".*\?from=" + encodeForUrl("2014-02-28T00:00:00Z") + "&to=" + encodeForUrl("2014-03-01T00:00:00Z") + "&format=json$");
+		var expectation = new RegExp(".*\?dateFrom=" + encodeForUrl("2014-02-28T00:00:00Z") + "&dateTo=" + encodeForUrl("2014-03-01T00:00:00Z") + "&format=json$");
 
 		expect(url).toMatch(expectation);
 	});
