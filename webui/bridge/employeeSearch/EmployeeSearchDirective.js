2877b24cdb6d45e7f249db57e307a8e2cff06bb7
Thomas Jansen <t.jansen@sap.com>
2014-08-12 08:23:05 +0200

    fix for mobile nr

--- webui/bridge/employeeSearch/EmployeeSearchDirective.js
+++ webui/bridge/employeeSearch/EmployeeSearchDirective.js
@@ -27,6 +27,7 @@ angular.module('bridge.employeeSearch').directive('bridge.employeeSearch', funct
             $http.get('https://ifp.wdf.sap.corp:443/sap/bc/zxa/FIND_EMPLOYEE_JSON?id=' + $item.BNAME + '&origin=' + location.origin).then(function (response) {
                 $scope.selectedEmployee = response.data.DATA;
                 $scope.selectedEmployee.TELNR = $scope.selectedEmployee.TELNR_DEF.replace(/ /g, '').replace(/-/g, '');                    
+                $scope.selectedEmployee.TELNR_MOB = $scope.selectedEmployee.TELNR_MOBILE.replace(/ /g, '').replace(/-/g, '');
 
                 $http.get('/bridge/employeeSearch/buildings.xml').then(function (response) {
                 var data = new X2JS().xml_str2json(response.data);            
