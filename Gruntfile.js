59bc3bdd90d246b6350121f663ceefca52b20f71
Thomas Jansen <t.jansen@sap.com>
2014-08-25 09:12:40 +0200

    check stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,8 +33,14 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.log.header = function() {};  
   grunt.loadNpmTasks('eslint-grunt');
 
+  grunt.registerTask('check-style', function(){
+    var output = grunt.task.run('eslint:checkstyle');
+    grunt.file.write('checkstyle.xml', output);
+  });
+
   grunt.registerTask('check', ['eslint:compact:all']);
-  grunt.registerTask('check-style', ['eslint:checkstyle:all']);
+  //grunt.registerTask('check-style', ['eslint:checkstyle:all']);
 };
