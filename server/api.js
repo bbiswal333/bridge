28b32e5deeb8b7671fd06e2da33265b8e62d5154
D051804 <daniel.schueler@sap.com>
2014-09-01 11:16:28 +0200

    changed cache, remove -> 'no-cache' to make it work in IE

--- server/api.js
+++ server/api.js
@@ -361,7 +361,8 @@ exports.register = function(app, user, local, proxy, npm, eTag, sso_enable)
 		var getResponse = true;		
 		if(eTag !== undefined)
 		{
-			response.setHeader('Cache-Control', 'no-cache, max-age=2592000');	// 30 days		
+			//response.setHeader('Cache-Control', 'no-cache, max-age=2592000');	// 30 days
+            response.setHeader('Cache-Control', 'must-revalidate, private');
 			response.setHeader('ETag', eTag);		
 			if( request.headers['if-none-match'] === eTag)
 			{
@@ -371,7 +372,8 @@ exports.register = function(app, user, local, proxy, npm, eTag, sso_enable)
 		else
 		{
 			response.header('Expires', '-1');
-			response.header('Cache-Control', 'no-cache, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+			//response.header('Cache-Control', 'no-cache, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+            response.header('Cache-Control', 'must-revalidate, max-stale=0, post-check=0, pre-check=0');
 			response.setHeader('Last-Modified', (new Date()).toUTCString());
 			response.removeHeader('Etag');
 		}	
