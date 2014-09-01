420a5213515475668c836645da2590835473dd95
Thomas Jansen <t.jansen@sap.com>
2014-09-01 15:52:10 +0200

    eslint errors in beta app

--- webui/app/beta_test/overview.js
+++ webui/app/beta_test/overview.js
@@ -1,10 +1,10 @@
 ﻿angular.module('app.betatest', []);
 angular.module('app.betatest').directive('app.betatest', function () {
 
-    var directiveController = ['$scope', '$window', 'notifier', function ($scope, $window, notifier)
+    var directiveController = function ()
     {
            //it would be okay for non beta testers to include a syntax error here as the angular module is not loaded and just the script tag is included
-    }];
+    };
 
     return {
         restrict: 'E',
