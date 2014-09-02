7c1b19eec9b48f43e830f2df6a917bcf136b0fa2
Thomas Jansen <t.jansen@sap.com>
2014-09-02 09:10:31 +0200

    package.json for badges

--- badge/prodStatusBadge.js
+++ badge/prodStatusBadge.js
@@ -2,6 +2,37 @@ var https   = require('https');
 var fs      = require('fs');
 var request = require('request');
 var path    = require('path');
+var exec    = require('child_process').exec;
+
+
+function loadnpm(proxy, npm, callback)
+{   
+    var server_path = path.join(__dirname, '/');    
+    var set_proxy = "";
+
+    if( proxy )
+    {
+        if(process.platform == "win32") {
+            set_proxy = "set http_proxy http_proxy=http://proxy:8080 && set https_proxy=http://proxy:8080 && ";
+        }
+        else {
+            set_proxy = "export http_proxy http_proxy=http://proxy:8080 && export https_proxy=http://proxy:8080 && ";
+        }
+    }
+
+    var server_modules = path.join(server_path, '/node_modules');
+    if(!fs.existsSync(server_modules))
+    {
+        fs.mkdirSync(server_modules);
+    }
+    
+    exec(set_proxy + 'cd "' + server_path + '" && ' + npm + ' install', function (error, stdout, stderr) {
+        console.log(stderr);        
+        callback();     
+    });
+    
+}
+
 
 function callBackend(hostname, port, path, method, callback) {
     var options = {
@@ -32,61 +63,65 @@ function callBackend(hostname, port, path, method, callback) {
 }
 
 
-callBackend('github.wdf.sap.corp', 443, '/api/v3/repos/bridge/bridge/tags', 'GET', function (data) 
+loadnpm(true,'npm', function()
 {
-    
-    if(data === undefined)
+    callBackend('github.wdf.sap.corp', 443, '/api/v3/repos/bridge/bridge/tags', 'GET', function (data) 
     {
-        console.log("GitHub not reachable");         
-    }
 
-    else
-    {        
-        var gitTags = JSON.parse(data);
-        var latest_tag = "v0.0";
         
-        //expecting format vX.Y for version
-        for (var i = 0; i < gitTags.length; i++) {
-            if (gitTags[i].name[1] > latest_tag[1] || (gitTags[i].name[1] == latest_tag[1] && parseInt(gitTags[i].name.substring(3)) > parseInt(latest_tag.substring(3)))) {
-                latest_tag = gitTags[i].name;
-            }
-        }
-        
-        console.log("Bridge Version on Server: " + latest_tag);
-
-        callBackend('github.wdf.sap.corp', 443, '/api/v3/repos/bridge/bridge/compare/' + latest_tag + '...master', 'GET', function (data)
+        if(data === undefined)
         {
-            var status = JSON.parse(data);
-            var count = status.ahead_by;     
+            console.log("GitHub not reachable");         
+        }
 
-            var color;
-            if (count === 0)
-            {
-                color = "green";
-            } 
-            else if (count > 0 && count <= 10) 
-            {
-                color = "yellow";                
-            } 
-            else 
-            {
-                color = "red";
+        else
+        {        
+            var gitTags = JSON.parse(data);
+            var latest_tag = "v0.0";
+            
+            //expecting format vX.Y for version
+            for (var i = 0; i < gitTags.length; i++) {
+                if (gitTags[i].name[1] > latest_tag[1] || (gitTags[i].name[1] == latest_tag[1] && parseInt(gitTags[i].name.substring(3)) > parseInt(latest_tag.substring(3)))) {
+                    latest_tag = gitTags[i].name;
+                }
             }
+            
+            console.log("Bridge Version on Server: " + latest_tag);
+
+            callBackend('github.wdf.sap.corp', 443, '/api/v3/repos/bridge/bridge/compare/' + latest_tag + '...master', 'GET', function (data)
+            {
+                var status = JSON.parse(data);
+                var count = status.ahead_by;     
+
+                var color;
+                if (count === 0)
+                {
+                    color = "green";
+                } 
+                else if (count > 0 && count <= 10) 
+                {
+                    color = "yellow";                
+                } 
+                else 
+                {
+                    color = "red";
+                }
 
 
-            var pictureUrl = "http://img.shields.io/badge/commits--behind-";
-            pictureUrl += count + '-' + color + ".svg";
+                var pictureUrl = "http://img.shields.io/badge/commits--behind-";
+                pictureUrl += count + '-' + color + ".svg";
 
-            function download(uri, filename, callback){
-                request.head(uri, function(){
-                    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                function download(uri, filename, callback){
+                    request.head(uri, function(){
+                        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                    });
+                }
+
+                download(pictureUrl, path.join(__dirname, "./prodstatus.svg"), function() {
+                    console.log("prodstatus picture created!");
                 });
-            }
 
-            download(pictureUrl, path.join(__dirname, "./prodstatus.svg"), function() {
-                console.log("prodstatus picture created!");
-            });
-
-        });    
-    }
+            });    
+        }
+    });
 });
