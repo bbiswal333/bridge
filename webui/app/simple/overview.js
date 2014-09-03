ba9c69a0501881452f291cbfb9cdcafdc3ed6da5
Rene Laengert <rene.laengert@sap.com>
2014-09-03 09:40:52 +0200

    Adding the simple app

---
+++ webui/app/simple/overview.js
@@ -0,0 +1,13 @@
+﻿angular.module('app.simple', []);
+angular.module('app.simple').directive('app.simple', function () {
+
+	var directiveController = ['$scope', function ($scope) {
+		$scope.appText = "Pretty simple app.";
+	}];
+
+	return {
+		restrict: 'E',
+		templateUrl: 'app/simple/overview.html',
+		controller: directiveController
+	};
+});
