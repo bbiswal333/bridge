d0b1db74de7383fff10dfe52bd4f35d1604ad8bc
D062131 <roman.kostka@sap.com>
2014-09-03 15:05:09 +0200

    checkbox + radio directive

---
+++ webui/bridge/controls/checkbox.js
@@ -0,0 +1,22 @@
+angular.module('bridge.app')
+  .directive('bridge.checkbox', function() {
+    return {
+      restrict: 'E',
+      template: '<label ng-class="{\'checkbox checked\': isChecked, \'checkbox\': !isChecked}" >' + 
+                  '<span class="icons">' + 
+                    '<span class="first-icon fa fa-square"></span>' +
+                    '<span class="second-icon fa fa-check-square"></span>' +
+                  '</span>' +
+                  '<input type="checkbox" ng-click="isChecked = !isChecked">' + 
+                  '<span class="content">' + 
+                    '<span class="unchecked-content">{{uncheckedLabel}}</span>' +
+                    '<span class="checked-content">{{checkedLabel}}</span>' +
+                '</span>' +
+              '</label>',
+      scope: {
+            checkedLabel: '@',
+            uncheckedLabel: '@',
+            isChecked: '@'
+      }
+    };
+  });
