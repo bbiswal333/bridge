4acdd10203f017b804c812808e411ca03a2dfa50
Anton <anton.kharitonov@sap.com>
2014-09-04 15:33:34 +0200

    initial upload of SAP News app

---
+++ webui/app/sapNews/overview.js
@@ -0,0 +1,23 @@
+﻿angular.module('app.sapNews', []);
+angular.module('app.sapNews').directive('app.sapNews', function () {
+
+    var directiveController = ['$scope', 'app.sapNews.feedService', function ($scope, feedService)
+    {
+        $scope.feeds = [];
+
+        function getFeedCallback(data){
+            $scope.feeds = data.channel.item;
+        }
+
+        //do only once:
+        if ($scope.feeds.length === 0) {
+            feedService.getNews().then(getFeedCallback);
+        }
+    }];
+
+    return {
+        restrict: 'E',
+        templateUrl: 'app/sapNews/overview.html',
+        controller: directiveController
+    };
+});
