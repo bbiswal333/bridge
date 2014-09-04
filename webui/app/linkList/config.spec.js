76150681d90f3d468e66f33183b8bfa99e78c66d
Richard Steuer <richard.steuer@sap.com>
2014-09-04 17:48:54 +0200

    [linklist] test coverage from 4.96% to 6.61%

---
+++ webui/app/linkList/config.spec.js
@@ -0,0 +1,30 @@
+/* global describe, beforeEach, it, expect, module, inject */
+describe("The linklist config service", function() {
+
+    "use strict";
+
+    var configService;
+
+    beforeEach(angular.mock.module("app.linklist"));
+
+    beforeEach(inject(["app.linklist.configservice", function(_configService_) {
+
+        configService = _configService_;
+
+
+    }]));
+
+    it("is not initialized on startup", function () {
+
+        expect(configService.isInitialized).toBe(false);
+
+    });
+
+    it("has a version and a listcollection as members", function () {
+
+        expect(configService.data.version).toBeDefined();
+        expect(configService.data.listCollection).toBeDefined();
+        
+    });
+
+});
