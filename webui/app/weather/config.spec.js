8d5c8a1712007575e7f821dac5e6be5f99cc2f49
Richard Steuer <richard.steuer@sap.com>
2014-09-04 15:45:40 +0200

    [weather] test coverage from 7.09% to 13.39%

---
+++ webui/app/weather/config.spec.js
@@ -0,0 +1,24 @@
+describe("The weather app config service", function () {
+
+	var configService, bridgeDataService;
+
+	beforeEach(module("app.weather"));
+	beforeEach(inject(["app.weather.configservice", "bridgeDataService", function (_configService_, _bridgeDataService_) {
+		 configService = _configService_;
+		 bridgeDataService = _bridgeDataService_;
+	}]));
+
+	it("should return the default config", function () {
+		expect(configService.configItem.fahrenheit).toBe(false);
+		expect(configService.configItem.location.name).toBe("Walldorf");
+	});
+
+	it("should try to call the bridgeDataService to get the user's building", function () {
+		
+		spyOn(bridgeDataService, 'getUserInfo');
+		configService.init();
+		expect(bridgeDataService.getUserInfo).toHaveBeenCalled();
+
+	});
+
+});
