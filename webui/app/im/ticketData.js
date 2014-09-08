be25f45b05bb65051e3cf3537c3ef726f047a19f
Rene Laengert <rene.laengert@sap.com>
2014-09-08 12:53:16 +0200

    removing trailing blanks

--- webui/app/im/ticketData.js
+++ webui/app/im/ticketData.js
@@ -12,11 +12,11 @@
     this.backendTickets.assigned_me = [];
     this.backendTickets.assigned_me_aa = [];
     this.backendTickets.created_me = [];
-    
+
     // make an object so that we can have it referenced in the scope
     this.isInitialized = { value: false };
 
-    function addTicket(list, ticket){ 
+    function addTicket(list, ticket){
         var allreadyExists = false;
         list.some(function(item){
             if (angular.equals(ticket, item)){
@@ -26,7 +26,7 @@
         });
 
         if (!allreadyExists){
-            list.push(ticket); 
+            list.push(ticket);
         }
     }
 
@@ -49,7 +49,7 @@
                     prio[category]++;
                     that.backendTickets[category].push(backendTicket);
                 }
-                
+
                 addTicket(prio.tickets, backendTicket);
                 prio.total = prio.tickets.length;
             }
@@ -58,9 +58,9 @@
 
     this.prios = [
         { name: "Very high",    number: 1, sel_components: 0, sel_components_aa: 0, colleagues:0, colleagues_aa: 0, assigned_me: 0, assigned_me_aa: 0, created_me: 0, selected: 0, total: 0, tickets: [] },
-        { name: "High",         number: 2, sel_components: 0, sel_components_aa: 0, colleagues:0, colleagues_aa: 0, assigned_me: 0, assigned_me_aa: 0, created_me: 0, selected: 0, total: 0, tickets: [] }, 
-        { name: "Medium",       number: 3, sel_components: 0, sel_components_aa: 0, colleagues:0, colleagues_aa: 0, assigned_me: 0, assigned_me_aa: 0, created_me: 0, selected: 0, total: 0, tickets: [] }, 
-        { name: "Low",          number: 4, sel_components: 0, sel_components_aa: 0, colleagues:0, colleagues_aa: 0, assigned_me: 0, assigned_me_aa: 0, created_me: 0, selected: 0, total: 0, tickets: [] }];    
+        { name: "High",         number: 2, sel_components: 0, sel_components_aa: 0, colleagues:0, colleagues_aa: 0, assigned_me: 0, assigned_me_aa: 0, created_me: 0, selected: 0, total: 0, tickets: [] },
+        { name: "Medium",       number: 3, sel_components: 0, sel_components_aa: 0, colleagues:0, colleagues_aa: 0, assigned_me: 0, assigned_me_aa: 0, created_me: 0, selected: 0, total: 0, tickets: [] },
+        { name: "Low",          number: 4, sel_components: 0, sel_components_aa: 0, colleagues:0, colleagues_aa: 0, assigned_me: 0, assigned_me_aa: 0, created_me: 0, selected: 0, total: 0, tickets: [] }];
 
     this.resetData = function () {
         angular.forEach(that.prios, function (prio) {
@@ -83,7 +83,7 @@
         that.backendTickets.assigned_me.length = 0;
         that.backendTickets.assigned_me_aa.length = 0;
         that.backendTickets.created_me.length = 0;
-    };    
+    };
 
     this.loadTicketData = function () {
         var deferred = $q.defer();
--- webui/app/imtps/overview.js
+++ webui/app/imtps/overview.js
@@ -1,41 +1,41 @@
 angular.module('app.imtps', ['ngTable']);
 
 angular.module('app.imtps').directive('app.imtps', ['app.imtps.configservice', function (appimtpsConfig) {
-	
+
 	var controller = ['$scope', 'app.imtps.configservice', 'app.imtps.msgReaderData','bridgeDataService', 'bridgeConfig',
 	    function Controller($scope, configService, msgReaderData, bridgeDataService, bridgeConfig) {
-		    
-	        $scope.box.boxSize = "1";      
+
+	        $scope.box.boxSize = "1";
 	        $scope.box.settingScreenData = {
 	            templatePath: "imtps/settings.html",
 	            controller: angular.module('app.imtps').appimtpsSettings,
 	            id: $scope.boxId
-	        };          
-	
+	        };
+
 	        $scope.box.returnConfig = function()
 	        {
 	            return configService;
 	        };
 
             $scope.box.reloadApp(msgReaderData.loadTicketData, 60 * 5);
-	        
+
 	        $scope.$watch('config', function (newVal, oldVal) {
 	            if($scope.config !== undefined && newVal !== oldVal)
-	            {                
+	            {
 	                // oldval is undefined for the first call of this watcher, i.e. the initial setup of the config. We do not have to save the config in this case
 	                if (oldVal !== undefined) {
 	                    bridgeConfig.persistInBackend(bridgeDataService);
 	                }
 	            }
 	        },true);
-		
+
 	        $scope.prioarray = [0,0,0,0];
 	        $scope.prionr = [1,2,3,4];
-			
+
 	        if (appimtpsConfig.isInitialized === false){
             	appimtpsConfig.initialize($scope.id);
             }
-	        
+
 			msgReaderData.initService( function(messages){
 				if( messages ){
 					$scope.prioarray = [0,0,0,0];
@@ -48,13 +48,13 @@ angular.module('app.imtps').directive('app.imtps', ['app.imtps.configservice', f
 							$scope.prioarray[2] = $scope.prioarray[2] + 1;
 						} else {
 							$scope.prioarray[3] = $scope.prioarray[3] + 1;
-						}					
+						}
 		            });
 				}
 	            if ( ($scope.prioarray[0] + $scope.prioarray[1] + $scope.prioarray[2] + $scope.prioarray[3]) === 0) {
 	                $scope.lastElement = "You have no internal messages to display!";
 	                $scope.displayChart = false;
-	            }                
+	            }
 	            else
 	            {
 	                $scope.displayChart = true;
@@ -62,7 +62,7 @@ angular.module('app.imtps').directive('app.imtps', ['app.imtps.configservice', f
 		});
 
 	}];
-	
+
     return {
         restrict: 'E',
         templateUrl: 'app/imtps/overview.html',
