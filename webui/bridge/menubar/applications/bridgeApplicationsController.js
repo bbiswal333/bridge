2fe20edaa7c2a87ead2c3694195da9c1a5c5eb0c
Christian Jeschke <christian.jeschke@sap.com>
2014-09-16 10:55:39 +0200

    It's now possible to add apps via the new menu bar Apps can now be categorized
    We need to discuss on categories and assign the apps accordingly

---
+++ webui/bridge/menubar/applications/bridgeApplicationsController.js
@@ -0,0 +1,29 @@
+angular.module('bridge.app').
+	controller('bridge.menubar.applicationsController',['$rootScope', '$scope', '$timeout', 'bridgeConfig','bridgeDataService',
+	function ($rootScope, $scope, $timeout, bridgeConfig, bridgeDataService){
+	    $scope.bridgeSettings = bridgeDataService.getBridgeSettings();
+	    $scope.apps = bridgeDataService.getProjects()[0].apps;
+	    $scope.categories = [{name: "All Apps", apps: []}];
+
+	    function findCategoryInList(name) {
+	    	for(var i = 0, length = $scope.categories.length; i < length; i++) {
+	    		if($scope.categories[i].name === name) {
+	    			return $scope.categories[i];
+	    		}
+	    	}
+	    	return false;
+	    }
+
+	    $scope.apps.map(function(app) {
+	    	if(app.metadata.categories) {
+	    		app.metadata.categories.map(function(category) {
+	    			if(!findCategoryInList(category)) {
+	    				$scope.categories.push({name: category, apps: [app]});
+	    			} else {
+						findCategoryInList(category).apps.push(app);
+	    			}
+	    		});
+	    	}
+	    	$scope.categories[0].apps.push(app);
+	    });
+}]);
--- webui/bridge/menubar/menubarDirective.js
+++ webui/bridge/menubar/menubarDirective.js
@@ -2,7 +2,7 @@ angular.module("bridge.app").directive("bridge.menubar", [function() {
 	return {
 		restrict: "E",
         templateUrl: "bridge/menubar/MenuBar.html",
-        controller: function($scope) {
+        controller: function($scope, $modal) {
         	$scope.displayView = function(view) {
         		if($scope.display === view) {
         			$scope.display = "";
@@ -10,6 +10,11 @@ angular.module("bridge.app").directive("bridge.menubar", [function() {
         		}
         		$scope.display = view;
         	};
+
+                $scope.changeSelectedApps = function() {
+                        $scope.toggleDragging();
+                        $scope.displayView('apps');
+                };
         }
 	};
 }]);
