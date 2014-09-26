e938764dedeef20dbba149a47d91071b4bb21250
D051804 <daniel.schueler@sap.com>
2014-09-26 13:00:28 +0200

    input directive created

---
+++ webui/bridge/controls/input.js
@@ -0,0 +1,24 @@
+angular.module('bridge.app').directive('bridge.input', function() {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'bridge/controls/input.html',
+        replace: true,
+        scope: {
+            placeholder: '@?',
+            model: '=?',
+            blur: '&',
+            focus: '&'
+        }
+        /*link:function ($scope, element, attrs) {
+            $scope.$parent.$watch(attrs.ngDisabled, function(newVal){
+                $scope.disabled = newVal;
+            });
+            $scope.checkClicked = function(){
+                if ($scope.disabled !== true) {
+                    $scope.isChecked = !$scope.isChecked;
+                }
+            };
+        }*/
+    };
+});
