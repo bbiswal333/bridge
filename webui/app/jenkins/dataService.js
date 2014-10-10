66917a87e4ee249fa488644ce0830364b81dae32
Priyanka Sakundarwar <priyanka.sakundarwar@sap.com>
2014-10-10 09:51:51 +0200

    status aggregation few more changes

--- webui/app/jenkins/dataService.js
+++ webui/app/jenkins/dataService.js
@@ -1,4 +1,4 @@
-angular.module('app.jenkins').service("app.jenkins.dataService", ["$http", "$q", "$log", function($http, $q, $log) {
+angular.module("app.jenkins").service("app.jenkins.dataService", ["$http", "$q", "$log", function($http, $q, $log) {
 
 	var that = this;
 
@@ -57,7 +57,7 @@ angular.module('app.jenkins').service("app.jenkins.dataService", ["$http", "$q",
 		if(!that.isValidUrl(jenkinsUrl)) {
 			that.initialize();
 		} else {
-			$http.get('/api/get?url=' + encodeURIComponent(jenkinsUrl + "/api/json?depth=1&tree=mode"), {withCredentials: false, timeout: 2000})
+			$http.get("/api/get?url=" + encodeURIComponent(jenkinsUrl + "/api/json?depth=1&tree=mode"), {withCredentials: false, timeout: 2000})
 			.success(function (data) {
 				if(angular.isDefined(data.mode)) {
 					that.jenkinsData.urlIsValid = true;
@@ -72,7 +72,7 @@ angular.module('app.jenkins').service("app.jenkins.dataService", ["$http", "$q",
 	this.getJenkinsViewsAndJobs = function(jenkinsUrl){
 		that.jenkinsData.viewsAreLoading = true;
 		that.jenkinsData.jobsAreLoading = true;
-		$http.get('/api/get?url=' + encodeURIComponent(jenkinsUrl + "/api/json?depth=1&tree=views[name,url],jobs[name,url,color]"), {withCredentials: false, timeout: 10000})
+		$http.get("/api/get?url=" + encodeURIComponent(jenkinsUrl + "/api/json?depth=1&tree=views[name,url],jobs[name,url,color]"), {withCredentials: false, timeout: 10000})
 		.success(function (data) {
 			if(angular.isDefined(data)) {
 				setJenkinsData(data);
@@ -86,7 +86,7 @@ angular.module('app.jenkins').service("app.jenkins.dataService", ["$http", "$q",
 
 	this.getJenkinsJobsForView = function(viewUrl){
 		that.jenkinsData.jobsAreLoading = true;
-		$http.get('/api/get?url=' + encodeURIComponent(viewUrl + "/api/json"), {withCredentials: false, timeout: 10000})
+		$http.get("/api/get?url=" + encodeURIComponent(viewUrl + "/api/json"), {withCredentials: false, timeout: 10000})
 		.success(function (data) {
 			if(angular.isDefined(data.jobs)) {
 				that.jenkinsData.jobsForView = data.jobs;
@@ -102,7 +102,7 @@ angular.module('app.jenkins').service("app.jenkins.dataService", ["$http", "$q",
 	};
 
 	function getLastBuildTimestamp(job) {
-		$http.get('/api/get?url=' + encodeURIComponent(job.jenkinsUrl + "/job/" + job.name + "/lastBuild/api/json?depth=1&tree=timestamp"), {withCredentials: false, timeout: 10000})
+		$http.get("/api/get?url=" + encodeURIComponent(job.jenkinsUrl + "/job/" + job.name + "/lastBuild/api/json?depth=1&tree=timestamp"), {withCredentials: false, timeout: 10000})
 		.success(function (data) {
 			job.timestamp = formatTimestamp(data.timestamp);
 			job.lastBuild = data.timestamp;
@@ -119,7 +119,7 @@ angular.module('app.jenkins').service("app.jenkins.dataService", ["$http", "$q",
 		job.lastbuildUrl = job.jenkinsUrl + "/job/" + job.name + "/lastBuild";
 		getLastBuildTimestamp(job);
 
-		$http.get('/api/get?url=' + encodeURIComponent(job.url + "/api/json?depth=1&tree=color"), {withCredentials: false, timeout: 10000})
+		$http.get("/api/get?url=" + encodeURIComponent(job.url + "/api/json?depth=1&tree=color"), {withCredentials: false, timeout: 10000})
 		.success(function (data) {
 			var color = data.color;
 			job.color = (color === "notbuilt") ? "grey" : color;
