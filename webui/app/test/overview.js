8c738b67daaec6ae45f43a50976f0d41912df986
Thorsten Sandfuchs <thorsten.sandfuchs@sap.com>
2014-10-28 14:54:41 +0100

    reverted changes in non-rooms part

--- webui/app/test/overview.js
+++ webui/app/test/overview.js
@@ -1,29 +1,14 @@
-angular.module('app.test', []);
-angular.module('app.test').directive('app.test', ['$http', function () {
+﻿angular.module('app.test', []);
+angular.module('app.test').directive('app.test', function () {
 
-    var directiveController = ['$scope', function ($scope, $http)
-    {/*
-        function _loadFromIsp(callback) {
-            $http({
-                    method: 'GET',
-                    url: window.client.origin + "/api/CalDataSSO"; 
-                }).success(function(data){
-        //data.RESERVATIONS.forEach(extractDates);
-                    alert(JSON.stringify(data));
-                    callback(data);
-      }).error(function(){
-          alert("error");
-      });
-    }
-        _loadFromIsp(function(data){console.log(data);});
-        */
+    var directiveController = ['$scope', function ($scope)
+    {
+        //put some stuff in here
     }];
-    
-    
-    
+
     return {
         restrict: 'E',
         templateUrl: 'app/test/overview.html',
         controller: directiveController
     };
-}]);
+});
