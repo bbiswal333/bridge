b2d7181811001fd057daf2be945d77d9fe535a26
Thorsten Sandfuchs <thorsten.sandfuchs@sap.com>
2014-11-03 08:21:09 +0100

    changes date from backend to be parsed as UTC. Fixes #603

--- webui/app/rooms/ifp.js
+++ webui/app/rooms/ifp.js
@@ -14,10 +14,10 @@ angular.module('app.rooms').service('ifpservice', [
 
             var datePattern = /^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/;
             var m = reservation.VALIDFROM.toString(10).match(datePattern);
-            reservation.VALIDFROMDATE = new Date(parseInt(m[1]),parseInt(m[2]) - 1,parseInt(m[3]),parseInt(m[4]),parseInt(m[5]));
-            m = reservation.VALIDTO.toString().match(datePattern);
-            reservation.VALIDTODATE = new Date(parseInt(m[1]),parseInt(m[2]) - 1,parseInt(m[3]),parseInt(m[4]),parseInt(m[5]));
-        }
+			reservation.VALIDFROMDATE = new Date(Date.UTC(parseInt(m[1]),parseInt(m[2]) - 1,parseInt(m[3]),parseInt(m[4]),parseInt(m[5])));
+			m = reservation.VALIDTO.toString().match(datePattern);
+			reservation.VALIDTODATE = new Date(Date.UTC(parseInt(m[1]),parseInt(m[2]) - 1,parseInt(m[3]),parseInt(m[4]),parseInt(m[5])));
+		}
 
         function extractRoom(reservation) {
             var regex = ".*\-(.*)[\/|\-].*\/(.*)$";
