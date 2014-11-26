f7873ca81c8c4448d9c14ab588f10fd98ba02bdb
Rene Laengert <rene.laengert@sap.com>
2014-11-26 16:04:22 +0100

    quick fix

--- webui/app/cats/catsMonthlyData.js
+++ webui/app/cats/catsMonthlyData.js
@@ -229,8 +229,10 @@ angular.module("app.cats.monthlyDataModule", ["lib.utils"])
 						if (task.UNIT !== 'H') {
 							// Adjusting to acutal part-time and country specific target hours value
 							var roundedTargetHours = Math.round(Math.round(day.targetHours / day.hoursOfWorkingDay * 1000) / 1000 * day.hoursOfWorkingDay * 1000) / 1000;
-							task.QUANTITY_DAY = Math.round(task.QUANTITY_DAY * day.hoursOfWorkingDay / roundedTargetHours * 1000) / 1000;
+						} else {
+							roundedTargetHours = day.targetHours;
 						}
+						task.QUANTITY_DAY = Math.round(task.QUANTITY_DAY * day.hoursOfWorkingDay / roundedTargetHours * 1000) / 1000;
 					}
 				});
 			});
