5bbd47fef4bc6a4786b46446c9e283e747ef56ef
I058984 <29Oct1983&>
2014-11-27 09:47:27 +0530

    review comments

--- webui/app/spotTheOdd/overview.js
+++ webui/app/spotTheOdd/overview.js
@@ -39,6 +39,7 @@ angular.module('app.spotTheOdd').directive('app.spotTheOdd', ['$http', 'app.spot
             $scope.gamePaused = false;
             $scope.showInfo = true;
 			$scope.box.boxSize = 2;
+			$scope.buttonText = 'Start Game';
         };
 
         $scope.init();
@@ -51,8 +52,8 @@ angular.module('app.spotTheOdd').directive('app.spotTheOdd', ['$http', 'app.spot
                     $scope.time--;
                     if ($scope.time <= 0) {
                         $scope.gameInProgress = false;
-                        //Adjust Box Size
-                        //$scope.box.boxSize = 1;
+						$scope.buttonText = 'Play Again';
+						
                         if (angular.isDefined(timer)) {
                             $interval.cancel(timer);
                             timer = undefined;
