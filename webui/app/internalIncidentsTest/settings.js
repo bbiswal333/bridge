822753a2e33353dcde0765e0f625b946df2399ea
Daniel Schueler <daniel.schueler@sap.com>
2014-12-01 09:45:00 +0100

    deleted internal incidents test-app

--- webui/app/internalIncidentsTest/settings.js
+++
@@ -1,9 +0,0 @@
-angular.module('app.internalIncidentsTest')
-    .controller('app.internalIncidentsTest.settingsController', ["$scope", "$http", "app.internalIncidentsTest.configservice", function($scope, $http, config){
-
-        $scope.config = config;
-
-        $scope.save_click = function () {
-            $scope.$emit('closeSettingsScreen', {app: 'itdirect'});
-        };
-}]);
