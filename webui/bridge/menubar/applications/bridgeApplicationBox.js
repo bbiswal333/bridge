0837110dce364896b5f9b1d1c45fda33b79f32f7
Christian Jeschke <christian.jeschke@sap.com>
2014-12-09 11:22:58 +0100

    implemented feedback fom Roman regarding number of instances per app + fixed typo in github description

--- webui/bridge/menubar/applications/bridgeApplicationBox.js
+++ webui/bridge/menubar/applications/bridgeApplicationBox.js
@@ -1,6 +1,11 @@
-angular.module("bridge.app").directive("bridge.application.box", [function() {
+angular.module("bridge.app").directive("bridge.application.box", ["bridge.service.appCreator", function(appCreator) {
 	return {
 		restrict: 'E',
-		templateUrl: "bridge/menubar/applications/bridgeApplicationBox.html"
+		templateUrl: "bridge/menubar/applications/bridgeApplicationBox.html",
+		controller: function($scope) {
+			$scope.numberOfInstances = function() {
+				return appCreator.getInstancesByType($scope.app.metadata.module_name).length;
+			};
+		}
 	};
 }]);
