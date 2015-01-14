13ebf9879fcfdf8f3e19b3cbd5818ff7141a657e
Patrick Schroth <p.schroth@sap.com>
2015-01-14 10:10:24 +0100

    Initial Commit BW Content Status OPP Apps
    Initial Commit BW Content Status OPP Apps

---
+++ webui/app/bwContentStatus/settings.js
@@ -0,0 +1,24 @@
+angular.module('app.bwContentStatus').appContentStatusSettings =
+	['$scope', "app.bwContentStatus.configService",'app.bwContentStatus.dataService',  function ($scope, configService, dataService) {
+
+	$scope.values = configService.values;
+	if ($scope.values.contents.length == 0 ) $scope.values.contents = dataService.data.configContents;
+	if ($scope.values.contents.length != dataService.data.configContents.length ) {
+		var tmp = angular.copy($scope.values.contents);
+		$scope.values.contents = dataService.data.configContents;
+
+		for (var i = 0; i < $scope.values.contents.length; i++) {
+			for (var n = 0; n < tmp.length; n++) {
+				if ($scope.values.contents[i].CONTID == tmp[n].CONTID && tmp[n].active) $scope.values.contents[i].active = true;
+			};
+		};
+	};
+
+	$scope.save_click = function () {
+		$scope.$emit('closeSettingsScreen', {app: 'bwContentStatus'}); // Persisting the settings
+	};
+
+	$scope.setActive = function (content) {
+		content.active = !content.active;
+	};
+}];
---
+++ webui/app/bwPcStatus/settings.js
@@ -0,0 +1,27 @@
+angular.module('app.bwPcStatus').appPcStatusSettings =
+	['$scope', "app.bwPcStatus.configService",'app.bwPcStatus.dataService',  function ($scope, configService, dataService) {
+
+	$scope.values = configService.values;
+	if ($scope.values.contents.length == 0 ) $scope.values.contents = dataService.data.configContents;
+	if ($scope.values.contents.length != dataService.data.configContents.length ) {
+		var tmp = angular.copy($scope.values.contents);
+		$scope.values.contents = dataService.data.configContents;
+
+		for (var i = 0; i < $scope.values.contents.length; i++) {
+			for (var n = 0; n < tmp.length; n++) {
+				if ($scope.values.contents[i].CONTID == tmp[n].CONTID && tmp[n].active) $scope.values.contents[i].active = true;
+			};
+		};
+	};
+
+	$scope.save_click = function () {
+		dataService.setAssignedContents(configService.values.contents).then(function() {
+			$scope.$emit('closeSettingsScreen', {app: 'bwPcStatus'}); // Persisting the settings
+		});
+	};
+
+	$scope.setActive = function (content) {
+		content.active = !content.active;
+	};
+	
+}];
