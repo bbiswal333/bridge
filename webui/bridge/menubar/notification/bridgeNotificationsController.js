d4a0e5a99a81b382535c618b31a6e9d92b543870
Daniel Schueler <daniel.schueler@sap.com>
2015-01-15 14:47:28 +0100

    notification settings now open by default if the notifications popups are turned off fixes #646

--- webui/bridge/menubar/notification/bridgeNotificationsController.js
+++ webui/bridge/menubar/notification/bridgeNotificationsController.js
@@ -2,28 +2,36 @@ angular.module('bridge.app').
 	controller('bridge.menubar.notificationsController',['$rootScope', '$location', '$scope', '$filter', '$timeout', 'bridgeConfig','bridgeDataService', "notifier",
 	function ($rootScope, $location, $scope, $filter, $timeout, bridgeConfig, bridgeDataService, notifier){
 
-		$scope.showPreferences = false;
-    	$scope.preferencesText = "Show Preferences";
+		$scope.notifications = notifier.allNotifications();
+		$scope.notificationPopupPermission = notifier.getPermission();
+
+		function openPreferences(){
+			$scope.showPreferences = true;
+			$scope.preferencesText = "Hide Preferences";
+		}
+		function closePreferences(){
+			$scope.showPreferences = false;
+			$scope.preferencesText = "Show Preferences";
+		}
 
 	    $scope.preferences_click = function(){
-	        $scope.showPreferences = !$scope.showPreferences;
 	        if ($scope.showPreferences){
-	            $scope.preferencesText = "Hide Preferences";
+	            closePreferences();
 	        } else {
-	            $scope.preferencesText = "Show Preferences";
-	        }
+				openPreferences();
+			}
 
 	    };
 
-        $scope.notifications = notifier.allNotifications();
-        $scope.notificationPopupPermission = notifier.getPermission();
-
         if($scope.notificationPopupPermission === true) {
             $scope.notificationPopupPermissisonButton = 'Active';
+			closePreferences();
         } else if ($scope.notificationPopupPermission === false){
             $scope.notificationPopupPermissisonButton = 'Denied';
+			closePreferences();
         } else if ($scope.notificationPopupPermission === 'pleaseAsk'){
             $scope.notificationPopupPermissisonButton = 'Click to Activate';
+			openPreferences();
         }
 
         $scope.requestNotificationPermission = function(){
