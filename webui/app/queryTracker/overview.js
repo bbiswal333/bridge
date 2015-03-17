915c2d579e191ec70d49d2712010dc54f06fd449
Thomas Jansen <t.jansen@sap.com>
2015-03-17 12:58:02 +0100

    beta version of query tracker

---
+++ webui/app/queryTracker/overview.js
@@ -0,0 +1,17 @@
+﻿angular.module('app.querytracker', []);
+angular.module('app.querytracker').directive('app.querytracker',['$http','bridgeDataService', function ($http, bridgeDataService) {
+
+	var directiveController = ['$scope', function ($scope) {
+
+		var userInfo = bridgeDataService.getUserInfo();
+		$http.get('https://vantgvmwin049.dhcp.pgdev.sap.corp/api/queries/' + userInfo.BNAME).success(function(data){
+			$scope.data = data;
+		});
+	}];
+
+	return {
+		restrict: 'E',
+		templateUrl: 'app/queryTracker/overview.html',
+		controller: directiveController
+	};
+}]);
