91fc5a1319754dde593ff8df60dac637063638ef
Thomas Jansen <t.jansen@sap.com>
2015-03-18 13:16:51 +0100

    query number for query tracker, sorting and marking of due queries

--- webui/app/queryTracker/detail.js
+++ webui/app/queryTracker/detail.js
@@ -4,6 +4,25 @@ angular.module('app.querytracker').controller('app.querytracker.detailController
 		var userInfo = bridgeDataService.getUserInfo();
 		$http.get('https://vantgvmwin049.dhcp.pgdev.sap.corp/api/queries/' + userInfo.BNAME).success(function(data){
 			$scope.data = data;
+			var current_date = new Date();
+			$scope.data.forEach(function (query){
+				var time_diff = Date.parse(query.Deadline) - current_date;
+				var days_to_deadline = Math.ceil(time_diff / (1000 * 3600 * 24));
+				if( days_to_deadline < 2)
+				{
+					query.deadline_marker = true;
+
+				}
+				else
+				{
+					query.deadline_marker = false;
+				}
+
+			});
+
+			$scope.data.sort(function(a, b){
+  				return new Date(a.Deadline) - new Date(b.Deadline);
+			});
 		});
 
 }]);
