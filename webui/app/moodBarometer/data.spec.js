63bef7d0698cdf60b33381d6e8bc1e992a3d2072
Christian Jeschke <christian.jeschke@sap.com>
2015-03-19 13:26:57 +0100

    fixing unit tests and several eslint errors

---
+++ webui/app/moodBarometer/data.spec.js
@@ -0,0 +1,21 @@
+describe("Testing moodBarometer.app data service", function () {
+	var dataService;
+
+	beforeEach(module("app.moodBarometer.data"));
+	beforeEach(inject(["app.moodBarometer.dataService", function (_dataService_) {
+		 dataService = _dataService_;
+	}]));
+
+	it("Should return the welcome text", function () {
+		expect(dataService.getText()).toBe("Text from DataService, app was refreshed 0 time(s).");
+		dataService.reload();
+		expect(dataService.getText()).toBe("Text from DataService, app was refreshed 1 time(s).");
+	});
+
+	it("Should return the times of reload", function () {
+		expect(dataService.getReloadCounter()).toBe(0);
+		dataService.reload();
+		expect(dataService.getReloadCounter()).toBe(1);
+	});
+
+});
---
+++ webui/app/moodBarometer/detail.spec.js
@@ -0,0 +1,22 @@
+describe("The detail controller", function () {
+
+	var scope, dataService, controllerService;
+
+	beforeEach(angular.mock.module("app.moodBarometer"));
+	beforeEach(inject(["app.moodBarometer.dataService", function (_dataService_) {
+		 dataService = _dataService_;
+	}]));
+	beforeEach(inject(function ($rootScope, $controller) {
+
+        scope = $rootScope.$new();
+        controllerService = $controller;
+
+        controllerService("app.moodBarometer.detailController", {$scope: scope});
+
+    }));
+
+	it("should set the text from the data service", function () {
+		expect(scope.text).toBe(dataService.getText());
+	});
+
+});
