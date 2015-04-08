fb1c3504fa67f353d8bbd5c4a3d64afe192e8c7a
D051804 <daniel.schueler@sap.com>
2015-04-08 15:42:46 +0200

    updated gruntfile to new nwjs version

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -6,26 +6,12 @@
 module.exports = function (grunt) {
     var utils = require('./utils')(grunt);
 
-    var webkitVersion = "0.9.2";
+    var webkitVersion = "0.12.0";
     var webkitInfo = {
-        winFilename: "node-webkit-v" + webkitVersion + "-win-ia32.zip",
-        macFilename: "node-webkit-v" + webkitVersion + "-osx-ia32.zip",
-        source: "http://dl.node-webkit.org/v" + webkitVersion + "/",
+        winFilename: "nwjs-v" + webkitVersion + "-win-x64.zip",
+        macFilename: "nwjs-v" + webkitVersion + "-osx-x64.zip",
+        source: "http://dl.nwjs.io/v" + webkitVersion + "/",
         destination: "webkit_download/",
-    };
-
-    var nodeVersion = "0.10.26";
-    var nodeInfo = {
-        winFilename: "node.exe",
-        macFilename: "node-v" + nodeVersion + "-darwin-x86.tar.gz",
-        source: "http://nodejs.org/dist/v" + nodeVersion + "/",
-        destination: "node_download/",
-    };
-
-    var npmVersion = "1.4.6";
-    var npmInfo = {
-        winFilename: "npm-" + npmVersion + ".zip",
-        source: "http://nodejs.org/dist/npm/",
     };
 
     grunt.initConfig({
@@ -45,7 +31,6 @@ module.exports = function (grunt) {
             win: {
                 files: [
                   { expand: true, cwd: 'app/', src: ['**'], dest: 'build/win' },
-                  { expand: true, cwd: 'node_download/win', src: ['**', '!*.zip'], dest: 'build/win/node' },
                   { expand: true, cwd: 'webkit_download/win/unzip', src: ['nw.exe', 'nw.pak', 'icudt.dll', ' libEGL.dll', ' libGLESv2.dll', 'credits.html'], dest: 'build/win' },
                 ]
             },
@@ -53,7 +38,6 @@ module.exports = function (grunt) {
                 files: [
                   { expand: true, cwd: 'webkit_download/mac/unzip/', src: ['node-webkit.app/**', 'credits.html'], dest: 'build/mac' },
                   { expand: true, src: ['app/**'], dest: 'build/mac/node-webkit.app/Contents/Resources' },
-                  { expand: true, cwd: 'node_download/mac/unzip', src: ['**'], dest: 'build/mac/node-webkit.app/Contents/Resources/app/node' },
                 ],
                 options: {
                     mode: '777',
@@ -74,7 +58,7 @@ module.exports = function (grunt) {
                     noProcess: 'true',
                 }
             },
-        },        
+        },
         clean: {
             build: ["build"],
             package: ["build/win/package.json", "build/mac/bridge.app/Contents/Resources/app.nw/package.json"],
@@ -95,19 +79,13 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('copynpm', function(){
-        grunt.task.run('shell:copy:' + nodeInfo.destination + 'mac/unzip/bin/npm:' + 'build/mac/bridge.app/Contents/Resources/app.nw/node/bin');
-    });
-
     grunt.registerTask('createDownloadFolder', 'Create the download folder for node and webkit', function () {
         grunt.file.mkdir(webkitInfo.destination + 'win');
         grunt.file.mkdir(webkitInfo.destination + 'mac');
-        grunt.file.mkdir(nodeInfo.destination + 'win');
-        grunt.file.mkdir(nodeInfo.destination + 'mac');
     });
 
     grunt.registerTask('downloadResources', 'Download Node and node-webkit for Mac and Win', function () {
-        done = this.async();
+        var done = this.async();
         var downloadFinished = [];
 
         var checkAndDownload = function (sourcePath, destinationFolder, filename, platform) {
@@ -120,18 +98,13 @@ module.exports = function (grunt) {
 
         checkAndDownload(webkitInfo.source, webkitInfo.destination, webkitInfo.winFilename, 'win');
         checkAndDownload(webkitInfo.source, webkitInfo.destination, webkitInfo.macFilename, 'mac');
-        checkAndDownload(nodeInfo.source, nodeInfo.destination, nodeInfo.winFilename, 'win');
-        checkAndDownload(npmInfo.source, nodeInfo.destination, npmInfo.winFilename, 'win'); // download into node folder
-        checkAndDownload(nodeInfo.source, nodeInfo.destination, nodeInfo.macFilename, 'mac');
 
         Q.all(downloadFinished).done(function () {
             var unzipFinished = [];
 
             // untar seems to be non-blocking (in contrary to unzipping), so do this first
-            unzipFinished.push(utils.untarFile(nodeInfo.destination + 'mac/' + nodeInfo.macFilename, nodeInfo.destination + 'mac/unzip'));
             unzipFinished.push(utils.unzipFile(webkitInfo.destination + 'win/' + webkitInfo.winFilename, webkitInfo.destination + 'win/unzip'));
             unzipFinished.push(utils.unzipFile(webkitInfo.destination + 'mac/' + webkitInfo.macFilename, webkitInfo.destination + 'mac/unzip'));
-            unzipFinished.push(utils.unzipFile(nodeInfo.destination + 'win/' + npmInfo.winFilename, nodeInfo.destination + 'win')); // unzip npm directly into the win/node folder
 
             Q.all(unzipFinished).done(function () {
                 done();
@@ -144,17 +117,13 @@ module.exports = function (grunt) {
         fs.chmodSync('build/mac/node-webkit.app', oldRights.mode);
     });
 
-    grunt.registerTask('copyAlias', 'bla', function () {
-        grunt.file.copy("node_download/mac/unzip/bin/npm", "npmCopy", { noProcess: "true" });
-    });
-
     grunt.loadNpmTasks('grunt-node-webkit-builder');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-rename');
     grunt.loadNpmTasks('grunt-shell');
 
-    grunt.registerTask('default', ['clean:build', 'createDownloadFolder', 'downloadResources', 'copy:win', 'copy:mac', 'setRights', 'rename:mac_rename', 'copynpm']);
+    grunt.registerTask('default', ['clean:build', 'createDownloadFolder', 'downloadResources', 'copy:win', 'copy:mac', 'setRights', 'rename:mac_rename']);
     grunt.registerTask('src', ['copy:src_only_win', 'copy:src_only_mac']);
     grunt.registerTask('deploy', ['default', 'clean:package', 'rename:package_rename']);
 };
