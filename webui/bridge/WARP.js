89117797cffb6d3838b1b671c51d3853dff926bd
Rene Laengert <rene.laengert@sap.com>
2015-04-16 09:37:51 +0200

    WARP counter difference production vs. non-production

--- webui/bridge/WARP.js
+++ webui/bridge/WARP.js
@@ -1,10 +1,19 @@
 var swa = {
-	pubToken: '3b086d85-047d-4532-b6ed-d2957722d900',
+	pubToken: '',
 	baseUrl: 'https://poc.warp.sap.com/tracker/',
 	visitorCookieDuration: 7776000000,
 	dntLevel: 1,
 	bannerEnabled: false
 };
+
+/*eslint-disable no-undef */
+if( document.location.hostname === "localhost" || document.location.hostname === "bridge-master.mo.sap.corp"){
+	swa.pubToken = '3b086d85-047d-4532-b6ed-d2957722d900';
+} else if( document.location.hostname === "bridge.mo.sap.corp"){
+/*eslint-enable no-undef */
+	swa.pubToken = 'c2b7d999-bf31-43d9-9259-68190a2b9135';
+}
+
 (function(){
 /*eslint-disable no-undef */
 	var d = document;
