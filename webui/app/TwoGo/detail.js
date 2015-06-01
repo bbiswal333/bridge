299ce1833d4f73c5de208c7310ce90e15c4a15b2
d059391 <melissa.herrmann@sap.com>
2015-06-01 17:21:43 +0200

    changed the links from the buttons now you wont have an emty site if a disabled Button is clicked

--- webui/app/TwoGo/detail.js
+++ webui/app/TwoGo/detail.js
@@ -2,17 +2,13 @@
  * Created by D059391 on 23.04.2015.
  */
 
-angular.module('app.TwoGo').controller('app.TwoGo.detailController', ['$scope', 'app.TwoGo.dataService','$window',
-    function Controller($scope, dataService,$window ) {
-$scope.setUrl = function(a){
-    debugger;
-    if(a === null){
-
-    }else{
-
-            $window.open(a);
-    }
-};
+angular.module('app.TwoGo').controller('app.TwoGo.detailController', ['$scope', 'app.TwoGo.dataService', '$window',
+    function Controller($scope, dataService, $window) {
+        $scope.setUrl = function (a) {
+            if (a != null) {
+                $window.open(a);
+            }
+        };
 
         if (dataService.getWhichLinkClicked() === 1) {
             $scope.Heading = "To Home Today";
