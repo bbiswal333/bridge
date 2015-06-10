9eff25aed93626dfc6e1cf04d23aba3e5688f39c
Rene Laengert <rene.laengert@sap.com>
2015-06-10 09:58:02 +0200

    [cats] part time rounding issue fixed for DEV2002C, relates to #858

--- webui/app/cats/catsMonthlyData.js
+++ webui/app/cats/catsMonthlyData.js
@@ -207,7 +207,8 @@ angular.module("app.cats.monthlyDataModule", ["lib.utils"])
 							} else {
 								this.days[task.WORKDATE].actualTimeInPercentageOfDay += task.QUANTITY;
 							}
-							this.days[task.WORKDATE].actualTimeInPercentageOfDay = Math.round(this.days[task.WORKDATE].actualTimeInPercentageOfDay * 100) / 100;
+							// This value must not be rounded!
+							this.days[task.WORKDATE].actualTimeInPercentageOfDay = this.days[task.WORKDATE].actualTimeInPercentageOfDay;
 
 							// That coding does not belong here...
 							var block = {};
