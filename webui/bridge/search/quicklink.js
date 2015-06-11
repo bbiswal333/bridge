85e6649fd967252a4c4ad0729e42f13237e212c1
Guilherme Dellagustin <guilherme.dellagustin@sap.com>
2015-06-11 10:38:21 +0200

    [search_quicklink] Call the service to get the list only once

--- webui/bridge/search/quicklink.js
+++ webui/bridge/search/quicklink.js
@@ -1,4 +1,29 @@
-angular.module('bridge.search').service('bridge.search.quicklinkSearch', ['$http', '$window', function ($http, $window) {
+angular.module('bridge.search').service('bridge.search.quicklinkSearch', ['$http', '$window', '$log', function ($http, $window, $log) {
+
+    function findInArray(query, quickLinks, resultArray) {
+      for (var i = 0; i < quickLinks.length; i++) {
+        if(quickLinks[i].search(query) >= 0) {
+          resultArray.push({title: quickLinks[i], link: 'https://portal.wdf.sap.corp/go/' + quickLinks[i]});
+        }
+      }
+    }
+
+    var quickLinks = {};
+    var quickLinksAvailable = true;
+
+    function getQuickLinks() {
+      var q = "https://portal.wdf.sap.corp/irj/servlet/prt/portal/prtroot/com.sap.sen.prg.quicklinks.inputfield.QLIFServlet";
+
+      return $http({method: "GET", url: q, withCredentials: false}).then(
+        function(response) {
+          quickLinks = response.data.split(',');
+        }, function(reason) {
+          $log.log('[search_quicklinks] Unable to read quicklink list');
+          quickLinksAvailable = false;
+        }
+      );
+    }
+
     this.getSourceInfo = function() {
         return {
             icon: "fa fa-bolt",
@@ -6,21 +31,26 @@ angular.module('bridge.search').service('bridge.search.quicklinkSearch', ['$http
             defaultSelected: true
         };
     };
+
     this.findMatches = function(query, resultArray) {
-    	var q = "https://portal.wdf.sap.corp/irj/servlet/prt/portal/prtroot/com.sap.sen.prg.quicklinks.inputfield.QLIFServlet";
+      // if it failed once it will most likely fail again
+      // (CORS restriction for instance)
+      // We don't want to keep calling the service every search then.
+      if(!quickLinksAvailable)
+        return;
 
-    	return $http({method: "GET", url: q, withCredentials: false}).then(
+      if(quickLinks.lenght > 0) {
+        findInArray(query, quicklinks, resultArray)
+      }
+      else {
+        return getQuickLinks().then(
           function(response) {
-          	var quicklinks = response.data.split(',');
-
-            for (var i = 0; i < quicklinks.length; i++) {
-              if(quicklinks[i].search(query) >= 0) {
-          	    resultArray.push({title: quicklinks[i], link: 'https://portal.wdf.sap.corp/go/' + quicklinks[i]});
-              }
-          	}
+            findInArray(query, quickLinks, resultArray)
           }
-      );
+        );
+      }
     };
+
     this.getCallbackFn = function() {
         return function(selectedItem) {
         	$window.open(selectedItem.link);
