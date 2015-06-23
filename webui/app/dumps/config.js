52411ade3772c4bd14c43fe224346d21901f553c
Dirk Baumgaertel <dirk.baumgaertel@sap.com>
2015-06-23 17:45:08 +0200

    Initial version for S/4 Dumps

---
+++ webui/app/dumps/config.js
@@ -0,0 +1,30 @@
+angular.module('app.dumps').service("app.dumps.configservice", ['bridgeDataService', function (bridgeDataService) {
+	var Config = function(oConfigItem) {
+		if(oConfigItem) {
+			this.configItem = oConfigItem;
+		} else {
+			this.configItem = {
+				boxSize : '1',
+				system : 'PROD',
+				components : '',
+			};
+		}
+
+		this.getUrl = function() {
+			return (this.configItem.system && this.configItem.system === "PROD") ? 'https://reporting.byd.sap.corp/sap' : 'https://vns.wdf.sap.corp/sap';
+		};
+
+		this.getSystemName = function() {
+			return ((this.configItem.system === "PROD") ? "Prod" : "Verilab" ).split(""); 
+		};
+	}
+
+	var instances = {};
+	this.getConfigForAppId = function(appId) {
+		if(instances[appId] === undefined) {
+			instances[appId] = new Config(bridgeDataService.getAppConfigById(appId));
+		}
+		return instances[appId];
+	};
+	
+}]);
