2deb3da8e5455f768c5c9a6b8d956e43afd74aa3
Kai Roesner <kai-michael.roesner@sap.com>
2015-06-25 17:19:22 +0200

    So far so good...

--- webui/app/myFirstApp/detail.js
+++ webui/app/myFirstApp/detail.js
@@ -1,5 +1,9 @@
-angular.module('app.myFirstApp').controller('app.myFirstApp.detailController',['$scope', 'app.myFirstApp.dataService',
-	function Controller($scope, dataService) {
-
-	$scope.text = dataService.getText();
-}]);
+angular.module('app.myFirstApp').controller(
+	'app.myFirstApp.detailController',
+	[
+		'$scope',
+		function Controller($scope) {
+			$scope.text = "whoa!";
+		}
+	]
+);
--- webui/app/myFirstApp/overview.js
+++ webui/app/myFirstApp/overview.js
@@ -1,21 +1,14 @@
-﻿angular.module('app.myFirstApp', ['app.myFirstApp.data']);
-angular.module('app.myFirstApp').directive('app.myFirstApp', ['app.myFirstApp.configService', 'app.myFirstApp.dataService', function (configService, dataService) {
+﻿angular.module('app.myFirstApp', []);
+angular.module('app.myFirstApp').directive('app.myFirstApp', ['app.myFirstApp.configService', function (configService) {
 
-	var directiveController = ['$scope', '$window', 'notifier', function ($scope, $window, notifier) {
+	var directiveController = ['$scope', '$window', function ($scope, $window) {
 
 		// Required information to get settings icon/ screen
-		$scope.box.settingsTitle = "Configure Test App";
+		$scope.box.settingsTitle = "Configure dict.cc app";
 		$scope.box.settingScreenData = {
 			templatePath: "myFirstApp/settings.html",
 				controller: angular.module('app.myFirstApp').appTestSettings,
 				id: $scope.boxId
-		};
-
-		$scope.many = dataService.getReloadCounter();
-
-		$scope.getData = function() {
-			dataService.reload();
-			$scope.many = dataService.getReloadCounter();
 		};
 
 		// Bridge framework function to enable saving the config
@@ -23,17 +16,6 @@ angular.module('app.myFirstApp').directive('app.myFirstApp', ['app.myFirstApp.co
 			return angular.copy(configService);
 		};
 
-		// Bridge framework function to take care of refresh
-		$scope.box.reloadApp($scope.getData,60);
-
-		// Example function for notifications
-		$scope.testNotification = function() {
-			notifier.showInfo("This is just a test",
-				"As the title says: nothing to do here :-)",
-				$scope.$parent.module_name,
-				function() {},
-				7000, null); // duration: -1 -> no timout; undefined -> 5000 ms as default
-		};
 	}];
 
 	var linkFn = function ($scope) {
