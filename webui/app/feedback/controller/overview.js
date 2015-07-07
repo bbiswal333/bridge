59e7d5e96b6688ba2c4c5e270c17d122e8e20237
D062653 <fabian.hilz@sap.com>
2015-07-07 16:30:07 +0200

    Feedback_App
    Improve the functions and requests

--- webui/app/feedback/controller/overview.js
+++ webui/app/feedback/controller/overview.js
@@ -1,7 +1,7 @@
 ﻿angular.module('app.feedback', ['ngTagsInput']);
 angular.module('app.feedback').directive('app.feedback', ['app.feedback.configService', function (configService) {
 
-    var directiveController = ['$scope', '$location', 'feedback', '$timeout', function ($scope, $location, feedback, $timeout) {
+    var directiveController = ['$scope', '$location', 'feedback','$interval', function ($scope, $location, feedback, $interval) {
         $scope.like_count = 1;
         $scope.liked = false;
         $scope.btn = true;
@@ -9,10 +9,10 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
         $scope.box.boxSize = 2;
         $scope.delay = '10';
         $scope.allAnswersArray = [];
-        $scope.myDisplayedAnswers = [];
         $scope.questionsArray = [];
         var start = 0;
         var p = 1;
+
 
         $scope.box.settingsTitle = "Personalize Your Feedback-App!";
         $scope.box.settingScreenData = {
@@ -21,6 +21,57 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
             id: $scope.boxId
 
         };
+
+        $interval($scope.displayAnswer = function () {
+            console.log("Mein Array beim display:");
+            console.log($scope.allAnswersArray);
+            var rand = (Math.random()) * ($scope.allAnswersArray.length);
+            $scope.answer = $scope.allAnswersArray[Math.floor(rand)].answer_text;
+            if ($scope.allAnswersArray[Math.floor(rand)].name == undefined) {
+                $scope.employee = "anonymous";
+            }
+            else {
+                $scope.employee = $scope.allAnswersArray[Math.floor(rand)].name;
+            }
+        },3000);
+
+        $scope.getAnswer = function () {
+            $scope.allAnswersArray = [];
+            while (p <= $scope.reply) {
+                console.log("Vor dem Request:");
+                console.log($scope.qId);
+                $.ajax({
+
+                    type: 'GET',
+                    //url:         'https://culturewall-demo.mo.sap.corp/api/2.0/answers?page='+p,
+                    url: '/api/get?url=' + encodeURIComponent('http://10.18.170.23:5000/api/2.0/answers?page=' + p + '&max_results=40&where={"question_id":' + '"' + $scope.qId + '"' + '}'),
+                    contentType: 'application/json; charset=UTF-8',
+                    dataType: 'json',
+                    timeout: 3000,
+                    retry_max: 3,
+                    async: false,
+                    success: function (data) {
+                        for (var i = 0; i < data._items.length; i++) {
+                            $scope.allAnswersArray.push(data._items[i]);
+                        }
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                });
+                p++;
+            }
+           $scope.displayAnswer();
+        };
+
+        $scope.setIndex = function () {
+            console.log("FragenArray:");
+            console.log($scope.questionsArray);
+            $scope.qId = $scope.questionsArray[start].question_ids;
+            $scope.question = $scope.questionsArray[start]._id;
+            $scope.getAnswer();
+        };
+
         $scope.getQuestion = function () {
             $.ajax({
                 type: 'GET',
@@ -29,16 +80,21 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
                 contentType: 'application/json; charset=UTF-8',
                 dataType: 'json',
                 timeout: 3000,
+                async: false,
                 retry_max: 3,
                 success: function (data) {
                     for (var i = 0; i < data.result.length; i++) {
                         $scope.questionsArray.push(data.result[i]);
+                        console.log("in der getQuestion-Schleife:");
+                        console.log($scope.questionsArray);
                     }
                 },
                 error: function (err) {
                     console.log(err);
                 }
             });
+            console.log("in der getQuestion:");
+            console.log($scope.questionsArray);
 
             $.ajax({
                 type: 'GET',
@@ -47,6 +103,7 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
                 contentType: 'application/json; charset=UTF-8',
                 dataType: 'json',
                 timeout: 3000,
+                async: false,
                 retry_max: 3,
                 success: function (data) {
                     console.log("Reply wurde gesetzt!");
@@ -57,73 +114,28 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
                     console.log(err);
                 }
             });
+            $scope.setIndex();
         };
-
-        $scope.getQuestion();
-        $timeout(function () {
-            $scope.qId = $scope.questionsArray[start].question_ids;
-            $scope.question = $scope.questionsArray[start]._id;
-        }, 150);
-
-        $timeout($scope.getAnswer = function () {
-            while (p <= $scope.reply) {
-                $.ajax({
-                    type: 'GET',
-                    //url:         'https://culturewall-demo.mo.sap.corp/api/2.0/answers?page='+p,
-                    url: '/api/get?url=' + encodeURI('http://10.18.170.23:5000/api/2.0/answers?page=' + p),
-                    contentType: 'application/json; charset=UTF-8',
-                    dataType: 'json',
-                    timeout: 3000,
-                    retry_max: 3,
-                    success: function (data) {
-                        console.log(data);
-                        for (var i = 0; i < data._items.length; i++) {
-
-                                $scope.allAnswersArray.push(data._items[i]);
-                        }
-                    },
-                    error: function (err) {
-                        console.log(err);
-                    }
-                });
-                p++;
-            }
-
-        }, 500);
-
-
-        $scope.tags = [{
-            text: "test"
-        }];
-
-        $scope.add = function () {
-            $location.path("/add/feedback");
-        };
-
         $scope.prevQuestion = function () {
             if (start == 0) {
                 var prev = start + ($scope.questionsArray.length - 1);
                 $scope.qId = $scope.questionsArray[prev].question_ids;
                 $scope.question = $scope.questionsArray[prev]._id;
-
+                p = 1;
                 $scope.getAnswer();
-                $scope.displayAnswer();
 
 
                 start = prev;
                 console.log(start);
                 console.log($scope.allAnswersArray);
-                console.log($scope.qId);
+
             }
             else {
                 start--;
                 $scope.qId = $scope.questionsArray[start].question_ids;
                 $scope.question = $scope.questionsArray[start]._id;
-
+                p = 1;
                 $scope.getAnswer();
-                $scope.displayAnswer();
-
-
                 console.log(start);
                 console.log($scope.allAnswersArray);
                 console.log($scope.qId);
@@ -135,9 +147,8 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
                 var next = 0;
                 $scope.qId = $scope.questionsArray[next].question_ids;
                 $scope.question = $scope.questionsArray[next]._id;
-
+                p = 1;
                 $scope.getAnswer();
-                $scope.displayAnswer();
 
                 start = next;
                 console.log(start);
@@ -148,13 +159,13 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
                 start++;
                 $scope.qId = $scope.questionsArray[start].question_ids;
                 $scope.question = $scope.questionsArray[start]._id;
-
+                p = 1;
                 $scope.getAnswer();
-                $scope.displayAnswer();
 
                 console.log(start);
                 console.log($scope.allAnswersArray);
                 console.log($scope.qId);
+
             }
         };
 
@@ -169,16 +180,17 @@ angular.module('app.feedback').directive('app.feedback', ['app.feedback.configSe
             }
         };
 
-        $timeout($scope.displayAnswer = function () {
-            var rand = (Math.random()) * ($scope.allAnswersArray.length);
-            $scope.answer = $scope.allAnswersArray[Math.floor(rand)].answer_text;
-            if ($scope.allAnswersArray[Math.floor(rand)].name == undefined) {
-                $scope.employee = "anonymous";
-            }
-            else {
-                $scope.employee = $scope.allAnswersArray[Math.floor(rand)].name;
-            }
-        }, 1000);
+        $scope.getQuestion();
+
+        $scope.tags = [{
+            text: "test"
+        }];
+
+        $scope.add = function () {
+            $location.path("/add/feedback");
+        };
+
+        $interval($scope.displayAnswer(),5000);
 
 
     }];
