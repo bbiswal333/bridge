680c217f1e76ccba8e103024727a9ecff80a428e
D062753 <philipp.de.sombre@sap.com>
2015-07-13 16:26:45 +0200

    moodBarometer styled
    improved details:
    all options will show up in details screen
    all mods now sorted in details screen

--- webui/app/moodBarometer/detail.js
+++ webui/app/moodBarometer/detail.js
@@ -1,18 +1,67 @@
-angular.module('app.moodBarometer').controller('app.moodBarometer.detailController',['$scope','$http','app.moodBarometer.configService','$window', 'app.moodBarometer.dataService',
-	function Controller($scope, $http, appConfig,$window, dataService) {
+angular.module('app.moodBarometer').controller('app.moodBarometer.detailController', ['$scope', '$http', 'app.moodBarometer.configService', '$window', 'app.moodBarometer.dataService',
+    function Controller($scope, $http, appConfig, $window, dataService) {
         $scope.text = dataService.getText();
 
         $scope.dataQ = [];
 
-        $scope.fill = function(data){
-            for(var i = 0; i < data.USERINFO.length; i++){
-               var a = { mood :     data.USERINFO[i].MOOD,
-                    count :     data.USERINFO[i].COUNT,
-                    perc :   $scope.getPercentage(data, i)};
-                $scope.dataQ.push(a);
+        $scope.allMoods = ['Happy', 'OK', 'Worried', 'Sad'];
+
+        $scope.index = function (a) {
+            switch (a.mood) {
+                case 'Happy':
+                    a.index = 0;
+                    break;
+                case 'OK':
+                    a.index = 1;
+                    break;
+                case 'Worried':
+                    a.index = 2;
+                    break;
+                case 'Sad':
+                    a.index = 3;
+                    break;
+                default :
+                    //console.log(a.mood);
+                    break;
             }
-            //console.log($scope.dataQ);
         };
+
+
+        $scope.fill = function (data) {
+            for (var i = 0; i < data.USERINFO.length; i++) {
+                var a = {
+                    mood: data.USERINFO[i].MOOD,
+                    count: data.USERINFO[i].COUNT,
+                    perc: $scope.getPercentage(data, i),
+                    index: null
+                };
+
+                $scope.index(a);
+                $scope.dataQ.push(a);
+
+
+
+                $scope.allMoods.splice($scope.dataQ[i].index, 1, null);
+            }
+
+            for (var i = 0; i < $scope.allMoods.length; i++) {
+                if ($scope.allMoods[i] != null) {
+                    var a = {
+                        mood: $scope.allMoods[i],
+                        count: 0,
+                        perc: 0,
+                        index: null
+                    };
+                    $scope.index(a);
+                    $scope.dataQ.push(a);
+                }
+
+            }
+
+
+        };
+
+
 
         $scope.getSum = function(data){
             var sum = 0;
