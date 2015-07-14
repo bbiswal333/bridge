acd6e7046aba312e582866b8985962a41fae0739
D062653 <fabian.hilz@sap.com>
2015-07-14 15:55:09 +0200

    Feedback_App
    Notification added

--- webui/app/feedback/controller/addAnswer.js
+++ webui/app/feedback/controller/addAnswer.js
@@ -1,19 +1,19 @@
 /**
  * Created by D062653 on 29.06.2015.
  */
-angular.module('app.feedback').controller('addCtrl', ['$scope', 'feedback', '$http', '$window', function ($scope, feedback, $http, $window) {
+angular.module('app.feedback').controller('addCtrl', ['$scope', 'feedback', '$http', '$window', "bridgeInBrowserNotification", "$location", function ($scope, feedback, $http, $window, bridgeInBrowserNotification, $location) {
     $scope.text2 = "Mein erster Satz!";
     $scope.maxLetters = 500;
     feedback.getQuestion($scope);
     $scope.values = feedback.values;
-    $scope.newAnswer = "";
+    $scope.currentLetters = "";
     $scope.add = function () {
-        var test = {
-                question_id: $scope.question_id[0],
-                answer_text: $scope.newAnswer
+        var postObject = {
+            question_id: $scope.question_id[0],
+            answer_text: $scope.currentLetters
         };
         $http({
-            url: 'api/post?&url='+encodeURIComponent('http://10.18.170.23:5000/api/2.0/answers'),
+            url: 'api/post?&url=' + encodeURIComponent('http://10.18.170.23:5000/api/2.0/answers'),
             method: "POST",
             headers: {
                 'Content-Type': 'application/json; charset=UTF-8'
@@ -21,14 +21,15 @@ angular.module('app.feedback').controller('addCtrl', ['$scope', 'feedback', '$ht
             dataType: 'json',
             timeout: 3000,
             retry_max: 3,
-            data: test
+            data: postObject
         }).success(function (res) {
-            console.log(res);
+            bridgeInBrowserNotification.addAlert('success', "Answer posted successfully -->" + "'" + postObject.answer_text + "'", 10);
+            $location.path("/");
         }).error(function (err) {
-            console.log(err);
+            bridgeInBrowserNotification.addAlert('danger', "Something went wrong: " + err, 10);
         });
     };
-    $scope.changeStatus = function(){
+    $scope.changeStatus = function () {
         $scope.values.anonym = !$scope.values.anonym;
     }
 }]);
