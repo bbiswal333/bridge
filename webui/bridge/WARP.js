04517ef7435a1818e3f3c77ff0c4372175de567f
Rene Laengert <rene.laengert@sap.com>
2015-08-19 12:58:44 +0200

    change the DNT (do not track) value for Web Analytics

--- webui/bridge/WARP.js
+++ webui/bridge/WARP.js
@@ -2,7 +2,7 @@ var swa = {
 	pubToken: '50e17cae-3311-4e74-96a1-4cef7e294256',
 	baseUrl: 'https://warp-ie.dmzmo.sap.corp/tracker/',
 	visitorCookieDuration: 7889232,
-	dntLevel: 1,
+	dntLevel: 3,
 	bannerEnabled: false
 };
 
@@ -12,7 +12,6 @@ if( document.location.hostname === "bridge-master.mo.sap.corp"){
 }
 
 (function(){
-/*eslint-disable no-undef */
 	var d = document;
 /*eslint-enable no-undef */
 	var g = d.createElement('script');
