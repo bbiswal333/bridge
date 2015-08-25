94037f672289f1ac0fa0afaf9dcc869f03d5c08d
D062653 <fabian.hilz@sap.com>
2015-08-25 15:45:21 +0200

    Delete for new Feature

--- webui/app/feedback/controller/settings.js
+++
@@ -1,9 +0,0 @@
-angular.module('app.feedback').appFeedbackSettings =
-	['$scope', "app.feedback.configService", function ($scope, configService) {
-
-	$scope.values = configService.values;
-
-	$scope.save_click = function () {
-		$scope.$emit('closeSettingsScreen'); // Persisting the settings
-	};
-}];
