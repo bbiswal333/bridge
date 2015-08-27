19ca1dc10a817cc824f36609d763afe0da15be5c
Guilherme Dellagustin <guilherme.dellagustin@sap.com>
2015-08-27 10:45:08 +0200

    [trafficLightService] Refactoring
    Some redundancies were removed and there was an unifaction of the term
    used for the trafficLight state.
    
    Before this patch both the terms state and color were used.
    Since the state Off have been introduced, the term state makes more sense
    since you can think of Green, Red and Yellow as states, but is does not
    fit so well to think of Off as color.

--- webui/bridge/trafficLightService.js
+++ webui/bridge/trafficLightService.js
@@ -3,71 +3,55 @@ angular.module('bridge.service').service('trafficLightService',
 
   var modulesState = [];
 
+	var states = {
+		'r': {
+			priority: 1,
+			name: 'Red'
+		},
+		'y': {
+			priority: 2,
+			name: 'Yellow'
+		},
+		'g': {
+			priority: 3,
+			name: 'Green'
+		},
+		'o': {
+			priority: 4,
+			name: 'Off'
+		}
+	};
+
 	function isClientOn(){
 		return $window.client.available;
 	}
 
-	function setColor(colorCode, colorName) {
-		$log.log('Traffic Light' + (isClientOn() ? ':' : ' (no client):')  + colorName);
+	function setState(state) {
+		$log.log('Traffic Light' + (isClientOn() ? ':' : ' (no client):')  + states[state].name);
 
 		if( isClientOn() ){
-			$http.get( $window.client.origin + '/api/trafficLight?color=' + colorCode );
+			$http.get( $window.client.origin + '/api/trafficLight?color=' + state );
 		}
 	}
-
-	function red(){
-		setColor('r', 'Red');
-	}
-
-	function yellow(){
-		setColor('y', 'Yellow');
-	}
-
-	function green(){
-		setColor('g', 'Green');
-	}
-
-  function off(){
-    setColor('o', 'Off');
-  }
 
   function updateTrafficLight() {
 
-		var color;
+		var state;
 
-		var colors = {
-			'R': {
-				priority: 1,
-				setter: red
-			},
-			'Y': {
-				priority: 2,
-				setter: yellow
-			},
-			'G': {
-				priority: 3,
-				setter: green
-			},
-			'O': {
-				priority: 4,
-				setter: off
-			}
-		};
-
-		color = colors.O;
+		state = 'o';
 
 		for(var key in modulesState)
 		{
-			if(colors[modulesState[key]].priority < color.priority) {
-				color = colors[modulesState[key]];
+			if(states[modulesState[key]].priority < states[state].priority) {
+				state = modulesState[key];
 			}
 		}
 
-		color.setter();
+		setState(state);
   }
 
   function setModuleState(appId, state) {
-    $log.log('Traffic Light for App ' + appId + ': ' + state );
+    $log.log('Traffic Light for App ' + appId + ': ' + states[state].name );
     modulesState[appId] = state;
     updateTrafficLight( );
   }
@@ -75,19 +59,19 @@ angular.module('bridge.service').service('trafficLightService',
   var TrafficLightServiceForApp = function(appId) {
 
     this.red = function() {
-      setModuleState(appId, 'R');
+      setModuleState(appId, 'r');
     };
 
     this.green = function() {
-      setModuleState(appId, 'G');
+      setModuleState(appId, 'g');
     };
 
     this.yellow = function() {
-      setModuleState(appId, 'Y');
+      setModuleState(appId, 'y');
     };
 
     this.off = function() {
-      setModuleState(appId, 'O');
+      setModuleState(appId, 'o');
     };
   };
 
@@ -96,5 +80,5 @@ angular.module('bridge.service').service('trafficLightService',
   };
 
 	// reset the TrafficLight at startup
-	off();
+	setState('o');
 }]);
