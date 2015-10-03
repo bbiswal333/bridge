421f8b1820d980ae694d4f96d68cc634dec7e27d
Yang Zhao <yang.zhao@sap.com>
2015-10-03 13:49:03 +0200

    Add links to quote and author

--- webui/app/agileCloud/overview.js
+++ webui/app/agileCloud/overview.js
@@ -1,7 +1,7 @@
 angular.module('app.agilecloud', []);
 angular.module('app.agilecloud').directive('app.agilecloud', function () {
 
-    var directiveController = ['$scope', 'app.agileCloud.dataService', function ($scope, dataService)
+    var directiveController = ['$scope', '$window', 'app.agileCloud.dataService', function ($scope, $window, dataService)
     {   // Update quotes randomly
 		$scope.updateQuote = function() {
 
@@ -15,12 +15,24 @@ angular.module('app.agilecloud').directive('app.agilecloud', function () {
 					$scope.author = quoteauthor[1];
 				} else {
 					$scope.quote  = quoteauthor[0];
-					$scope.author = '';
+					$scope.author = 'Voice of ASE@SAP';
 				}
 			});
 		};
 		$scope.updateQuote();
 		$scope.box.reloadApp($scope.updateQuote, 60 * 60);	// 1 hour
+
+		// Open google search window
+		$scope.googleQuote = function() {
+			$window.open('https://google.com/#q=' + $scope.quote);
+		}
+		$scope.googleAuthor = function() {
+			if ($scope.author === 'Voice of ASE@SAP') {
+				$window.open('https://go.sap.corp/ase');
+å			} else {
+				$window.open('https://google.com/#q=' + $scope.author);
+			}
+		}		
     }];
 
     return {
