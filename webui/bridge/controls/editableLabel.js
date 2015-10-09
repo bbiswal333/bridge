4370254c60877ac2e3c4f094b4f1e402c3955931
D051804 <daniel.schueler@sap.com>
2015-10-09 10:06:59 +0200

    [atc] fixed in-table-editing of systems in the app settings

--- webui/bridge/controls/editableLabel.js
+++ webui/bridge/controls/editableLabel.js
@@ -8,6 +8,16 @@ angular.module('bridge.app').directive('bridge.editableLabel', [function() {
             ownEditControl: '='
         },
         controller: ['$scope', function($scope) {
+            // we cannot bind $scope.text directly to the model of Bridge-Input, because it is a simple string and
+            // would be passed as a copy and thus not get updated. Wrap it in an object, so that it is passed as a reference
+            $scope.oPassToBridgeInput = {
+                text: $scope.text
+            };
+
+            $scope.$watch("oPassToBridgeInput", function(){
+                $scope.text = $scope.oPassToBridgeInput.text;
+            }, true);
+
         	$scope.editMode = false;
         	$scope.toggleEditMode = function() {
         		$scope.editMode = !$scope.editMode;
