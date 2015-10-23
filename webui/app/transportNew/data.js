760da7dafcf549615823ed569b6390b4164154b4
Christian Jeschke <christian.jeschke@sap.com>
2015-10-23 08:36:16 +0200

    don't load data if theres no config

--- webui/app/transportNew/data.js
+++ webui/app/transportNew/data.js
@@ -13,28 +13,34 @@ angular.module("app.transportNew")
 			return function() {
 				this.loadData = function(config) {
 					var deferred = $q.defer();
-					var url = 'https://ifp.wdf.sap.corp/sap/bc/bridge/GET_TRANSPORTS?components=' + config.components.join(";") + '&systems=' + config.systems.join(";") + '&owners=' + config.owners.map(function(owner) { return owner.selector; }).join(';') + '&origin=' + $window.location.origin;
-					var that = this;
-					var thresholdDaysAgo = new Date(new Date().setDate(new Date().getDate() - config.openTransportThreshold));
-					thresholdDaysAgo.setHours(0);
-					thresholdDaysAgo.setMinutes(0);
-					thresholdDaysAgo.setSeconds(0);
-					thresholdDaysAgo.setMilliseconds(0);
-					$http.get(url).success(function(data){
-						that.openTransports = [];
-						that.transportsOpenForLongerThanThreshold = [];
-						data.TRANSPORTS.map(function(transport) {
-							transport.FIRST_OCCURENCE = transport.FIRST_OCCURENCE.substring(0, 4) + "-" + transport.FIRST_OCCURENCE.substring(4, 6) + "-" + transport.FIRST_OCCURENCE.substring(6, 8);
-							if(!transport.PARENT_TRANSPORT && (transport.TYPE === "K" || transport.TYPE === "W" || transport.TYPE === "C" || transport.TYPE === "O" || transport.TYPE === "E" || transport.TYPE === "T" || transport.TYPE === "M" || transport.TYPE === "L")) {
-								that.openTransports.push(transport);
-
-								if(config.openTransportThreshold && toDate(transport.FIRST_OCCURENCE) < thresholdDaysAgo) {
-									that.transportsOpenForLongerThanThreshold.push(transport);
-								}
-							}
-						});
+					if(config.components.length === 0 && config.systems.length === 0 && config.owners.length === 0) {
+						this.openTransports = [];
+						this.transportsOpenForLongerThanThreshold = [];
 						deferred.resolve();
-					});
+					} else {
+						var url = 'https://ifp.wdf.sap.corp/sap/bc/bridge/GET_TRANSPORTS?components=' + config.components.join(";") + '&systems=' + config.systems.join(";") + '&owners=' + config.owners.map(function(owner) { return owner.selector; }).join(';') + '&origin=' + $window.location.origin;
+						var that = this;
+						var thresholdDaysAgo = new Date(new Date().setDate(new Date().getDate() - config.openTransportThreshold));
+						thresholdDaysAgo.setHours(0);
+						thresholdDaysAgo.setMinutes(0);
+						thresholdDaysAgo.setSeconds(0);
+						thresholdDaysAgo.setMilliseconds(0);
+						$http.get(url).success(function(data){
+							that.openTransports = [];
+							that.transportsOpenForLongerThanThreshold = [];
+							data.TRANSPORTS.map(function(transport) {
+								transport.FIRST_OCCURENCE = transport.FIRST_OCCURENCE.substring(0, 4) + "-" + transport.FIRST_OCCURENCE.substring(4, 6) + "-" + transport.FIRST_OCCURENCE.substring(6, 8);
+								if(!transport.PARENT_TRANSPORT && (transport.TYPE === "K" || transport.TYPE === "W" || transport.TYPE === "C" || transport.TYPE === "O" || transport.TYPE === "E" || transport.TYPE === "T" || transport.TYPE === "M" || transport.TYPE === "L")) {
+									that.openTransports.push(transport);
+
+									if(config.openTransportThreshold && toDate(transport.FIRST_OCCURENCE) < thresholdDaysAgo) {
+										that.transportsOpenForLongerThanThreshold.push(transport);
+									}
+								}
+							});
+							deferred.resolve();
+						});
+					}
 					return deferred.promise;
 				};
 			};
