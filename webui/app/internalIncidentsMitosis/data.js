4b195e20c6dd947d1286c028bd421dd910b088f6
Christian Jeschke <christian.jeschke@sap.com>
2015-12-02 15:26:30 +0100

    changed data retrieval for incidents to limited xsjs service

--- webui/app/internalIncidentsMitosis/data.js
+++ webui/app/internalIncidentsMitosis/data.js
@@ -19,54 +19,28 @@ angular.module("app.internalIncidentsMitosis")
 	        	return new Date(parseInt(match)).toISOString().replace(/[^0-9]/g, "");
 	        }
 
+	        function addSystems(program) {
+	        	var result = "";
+	        	if(program.SYSTEMS.length > 0) {
+	        		result += ";" + program.SYSTEMS.join(";");
+	        	}
+	        	return result;
+	        }
+
 	        function getFilterFromConfig(config) {
 	        	var filter = "";
-	        	if(config.systems.length > 0 || config.programs.length > 0 || config.processors.length > 0) {
-	        		filter += "( (";
-	        		filter += config.systems.map(function(system) {
-	        			return "II_SYSTEM_ID eq '" + system + "'";
-	        		}).join(' or ');
-	        		if(config.processors.length > 0) {
-	        			if(config.systems.length > 0) {
-	        				filter += " or ";
-	        			}
-	        			filter += config.processors.map(function(processor) {
-	        				return "II_PROCESSOR_ID eq '" + processor.BNAME + "'";
-	        			}).join(' or ');
-	        		}
-	        		if((config.systems.length > 0 || config.processors.length > 0) && config.programs.length > 0) {
-	        			filter += ") or (";
-	        		}
-	        		filter += config.programs.map(function(program) {
-	        			var result = "(TP_PROGRAM eq '" + program.TP_PROGRAM + "'";
-	        			if(program.SYSTEMS && program.SYSTEMS.length > 0) {
-	        				result += " and (" + program.SYSTEMS.map(function(system) {
-	        					return "II_SYSTEM_ID eq '" + system + "'";
-	        				}).join(" or ");
-	        				result += ")";
-	        			}
-	        			result += ")";
-	        			return result;
-	        		}).join(' or ');
-	        		filter += ")";
-					if(config.components.length > 0) {
-						filter += ") and ";
-					} else {
-						filter += ")";
-					}
-	        	}
-	        	if(config.components.length > 0) {
-	        		filter += "(";
-	        		filter += config.components.map(function(component) {
-	        			if(component.indexOf('*') >= 0) {
-	        				return "startswith(II_CATEGORY, '" + component.replace(/\*/gi, '') + "')";
-	        			} else {
-	        				return "II_CATEGORY eq '" + component + "'";
-	        			}
-	        		}).join(' or ');
-	        		filter += ")";
-	        	}
-	        	filter += " and II_STATUS_ID ne 'E0009' and II_STATUS_ID ne 'E0013' and II_STATUS_ID ne 'E0005'";
+	        	filter += "programFilter=";
+	        	filter += config.programs.map(function(program) {
+	        		return program.TP_PROGRAM + addSystems(program);
+	        	}).join("|");
+	        	filter += "&systemFilter=";
+	        	filter += config.systems.join(";");
+
+	        	filter += "&processorFilter=";
+	        	filter += config.processors.map(function(processor) { return processor.BNAME; }).join(";");
+
+	        	filter += "&componentFilter=";
+	        	filter += config.components.join(";");
 				return filter;
 	        }
 
@@ -91,13 +65,15 @@ angular.module("app.internalIncidentsMitosis")
 						that.prio4 = [];
 						deferred.resolve();
 					} else {
-						var url = "https://mithdb.wdf.sap.corp/irep/reporting/bridge/internalIncidents/incidents.xsodata/Incidents?$format=json&$filter=" + getFilterFromConfig(config) + "&origin=" + $window.location.origin;
+						var url = "https://mithdb.wdf.sap.corp/irep/reporting/internalIncidents/incidents.xsjs?" + getFilterFromConfig(config) + "&origin=" + $window.location.origin;
 						$http.get(url).success(function(data){
+							that.limit = data.limit;
+							that.limitExceeded = data.limitExceeded;
 							that.prio1 = [];
 							that.prio2 = [];
 							that.prio3 = [];
 							that.prio4 = [];
-							data.d.results.map(function(incident) {
+							data.incidents.map(function(incident) {
 								switch(incident.II_PRIORITY_TEXT) {
 									case "1-Very High":
 										that.prio1.push(transform(incident));
