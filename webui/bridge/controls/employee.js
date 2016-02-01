2df576b9526aa7fe0cea2ca022d1056cca9b97d3
Christian Jeschke <christian.jeschke@sap.com>
2016-02-01 15:07:03 +0100

    replaced custom prio boxes by a common control

--- webui/bridge/controls/employee.js
+++ webui/bridge/controls/employee.js
@@ -5,12 +5,33 @@ angular.module('bridge.app').directive('bridge.employee', [function() {
         scope: {
         	id: '=',
             mailBody: '@',
-            mailSubject: '@'
+            mailSubject: '@',
+            attachTo: '='
         },
         controller: ['$scope', 'employeeService', function($scope, employeeService) {
-    		employeeService.getData($scope.id).then(function(data) {
-    			$scope.employee = data;
-    		});
+            function loadEmployeeData() {
+                employeeService.getData($scope.id).then(function(data) {
+                    if(data.BNAME === "") {
+                        data.BNAME = $scope.id;
+                        data.NACHN = $scope.id;
+                        data.VORNA = $scope.id;
+                        data.fullName = $scope.id;
+                    }
+
+                    $scope.employee = data;
+                    if($scope.attachTo) {
+                        $scope.attachTo.employee = $scope.employee;
+                    }
+                });
+            }
+
+            loadEmployeeData();
+
+            $scope.$watch("id", function(newValue, oldValue) {
+                if(newValue !== oldValue) {
+                    loadEmployeeData();
+                }
+            });
         }],
         templateUrl: 'bridge/controls/employee.html'
     };
