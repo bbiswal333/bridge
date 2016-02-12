7025387de391b6aae104f15645ebd64cc0038c40
Matthias Scudlik <matthias.scudlik@sap.com>
2016-02-12 17:41:14 +0100

    use URL generated in backend

--- webui/bridge/search/pam.js
+++ webui/bridge/search/pam.js
@@ -6,17 +6,17 @@ angular.module('bridge.search').service('bridge.search.pamSearch', ['$http', '$w
         };
     };
     this.findMatches = function(query, resultArray) {
-		return $http.get('https://i7d.wdf.sap.corp/sap/opu/odata/SAP/ZMS_PAM_BRIDGE_SEARCH_SRV/ProductVersions?$format=json&search=*' + query + '*&sort=most_viewed%7Cdesc&origin=' + $window.location.origin).then(
+		return $http.get('https://i7d.wdf.sap.corp/sap/opu/odata/SAP/ZMS_PAM_BRIDGE_SEARCH_SRV/ProductVersions?$format=json&search=*' + query + '*&origin=' + $window.location.origin).then(
             function(response) {
             	response.data.d.results.map(function(result) {
-            		resultArray.push({title: result.Name, originalResult: result});
+            		resultArray.push({title: result.DisplayName, /*description: result.OfficialName, */ originalResult: result});
             	});
             }
         );
     };
     this.getCallbackFn = function() {
         return function(selectedItem) {
-            $window.open("https://i7d.wdf.sap.corp/sap/support/pam?hash=pvnr%3D" + selectedItem.originalResult.Id);
+            $window.open(selectedItem.originalResult.Url);
         };
     };
 }]);
