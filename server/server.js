12ef7f6c9cef30c4014957b6cea075e26efc53e8
Christian Jeschke <christian.jeschke@sap.com>
2016-03-07 07:46:09 +0100

    restoring correct rewrite rules

--- server/server.js
+++ server/server.js
@@ -31,8 +31,8 @@ exports.run = function(npm, port) {
 		}
 
 		var rules = [
-			//"-teams.mo.sap.corp ^\/$ /teamMigration/index.html",
-			//"-teams.mo.sap.corp ^\/index.html$ /teamMigration/index.html",
+			"-teams.mo.sap.corp ^\/$ /teamMigration/index.html",
+			"-teams.mo.sap.corp ^\/index.html$ /teamMigration/index.html",
 			"-teams.mo.sap.corp /migrationTest /teamMigration/index.html",
 			"localhost /migrationTest /teamMigration/index.html"
 		];
