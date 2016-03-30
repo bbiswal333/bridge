711bf8c78446f59ba9e64d75c897151248d1f6e6
Martina Dukadinova <martina.dukadinova@sap.com>
2016-03-30 16:42:04 +0200

    editView added

--- webui/app/notification/edit.js
+++ webui/app/notification/edit.js
@@ -1,14 +1,37 @@
+// angular.module('app.notification', ['ui.bootstrap.alert']);
 angular.module('app.notification').edit = ['$scope', '$modalInstance', 'notification', function($scope, $modalInstance, notification) {
 
     $scope.notification = notification;
+    
+    $scope.editMode = true;
 
-    $scope.save = function() {
-        // $scope.$emit('closeEditScreen');
-        $modalInstance.close();
+    $scope.publish = function() {
+        var emptyField = false;
+        if (notification.HEADER === undefined || notification.HEADER === '') {
+            // border color red
+            emptyField = true;
+        }
+        if (notification.PREVIEW === undefined || notification.PREVIEW === '') {
+            // border color red
+            emptyField = true;
+        }
+        if (notification.CONTENT === undefined || notification.CONTENT === '') {
+            // border color red
+            emptyField = true;
+        }
+
+        if (!emptyField) {
+            $modalInstance.close();
+        }
     };
 
     $scope.close = function() {
-        // $scope.$emit('closeEditScreen');
+        $scope.alert = { type: 'danger', msg: 'Your changes will be lost.' };
+
+        // $modalInstance.close();
+    };
+
+    $scope.view = function() {
         $modalInstance.close();
     };
 }];
