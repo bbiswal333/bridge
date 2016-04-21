30890a445e6e1296e2280fe6c85e9b8d07a3139b
D051804 <daniel.schueler@sap.com>
2016-04-21 15:28:23 +0200

    changed config utils so that we can remove the visibilty properties from the table configs in future

--- webui/bridge/ticketAppUtils/configUtils.js
+++ webui/bridge/ticketAppUtils/configUtils.js
@@ -5,7 +5,7 @@ angular.module("bridge.ticketAppUtils").service("bridge.ticketAppUtils.configUti
         var property;
 
         for (property in oBackendConfig) {
-            if (property === "columnVisibility" && oTarget.columnVisibility.length !== oBackendConfig.columnVisibility.length) {
+            if (property === "columnVisibility" && oTarget.columnVisibility !== undefined && oTarget.columnVisibility.length !== oBackendConfig.columnVisibility.length) {
                 // if the length of the columnVisibility attribute changed, reset to default. This happens for example if a new column is introduced
                 continue;
             } else {
